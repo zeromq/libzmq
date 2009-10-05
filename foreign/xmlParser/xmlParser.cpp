de2e439a52e93a4cb17a19e9ef9cadfd5ebf2cea
malosek <malosek@fastmq.com>
2009-10-05 15:06:08 +0200

    1 minor change in xmlParser.cpp

--- foreign/xmlParser/xmlParser.cpp
+++ foreign/xmlParser/xmlParser.cpp
@@ -225,7 +225,7 @@ char myIsTextWideChar(const void *b, int len) { return FALSE; }
         return FALSE;
     }
     #else
-    char myIsTextWideChar(const void *b,int l) { return (char)IsTextUnicode((CONST LPVOID)b,l,NULL); };
+    char myIsTextWideChar(const void *b,int l) { return (char)IsTextUnicode((CONST LPVOID)b,l,NULL); }
     #endif
 #endif
 
