30076c4f7a5b0c70ef0c5a6574cac531bc996f5e
Martin Sustrik <sustrik@250bpm.com>
2010-02-04 17:46:47 +0100

    ZMQII-58: TCP connecting asserts on Win32 (same fix for POSIX)

--- src/tcp_connecter.cpp
+++ src/tcp_connecter.cpp
@@ -275,7 +275,7 @@ zmq::fd_t zmq::tcp_connecter_t::connect ()
 
         //  Assert that the error was caused by the networking problems
         //  rather than 0MQ bug.
-        zmq_assert (err == ECONNREFUSED);
+        zmq_assert (err == ECONNREFUSED || err == ETIMEDOUT);
 
         errno = err;
         return retired_fd;
