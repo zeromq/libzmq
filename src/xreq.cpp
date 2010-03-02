e34184acc327ae4b085a1a50ff6502e2dc148522
Martin Hurton <hurtonm@gmail.com>
2010-03-02 11:15:50 +0100

    Implement flow control for ZMQ_XREQ sockets

--- src/xreq.cpp
+++ src/xreq.cpp
@@ -65,7 +65,7 @@ void zmq::xreq_t::xrevive (class reader_t *pipe_)
 
 void zmq::xreq_t::xrevive (class writer_t *pipe_)
 {
-    zmq_not_implemented ();
+    lb.revive (pipe_);
 }
 
 int zmq::xreq_t::xsetsockopt (int option_, const void *optval_,
