8fcf6ffb67aba2ff9f78f4ba327d29755f03535a
Martin Lucina <mato@kotelna.sk>
2010-03-15 15:47:17 +0100

    Cleanups to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -32,7 +32,7 @@ fi
 
 command -v autoreconf >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "autogen.sh: error: could not find autoreconf.  autoconf and automake are required to run autogen.sh."
+    echo "autogen.sh: error: could not find autoreconf.  autoconf and automake are required to run autogen.sh." 1>&2
     exit 1
 fi
 
