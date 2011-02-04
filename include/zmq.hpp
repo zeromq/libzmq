042e34a5d99a9d39b0680d063878f13f5d7e7337
Thijs Terlouw <thijsterlouw@gmail.com>
2011-02-04 16:44:13 +0100

    operator void* () added to context_t.
    Makes it possible to share the context from C++ to C.
    
    Signed-off-by: Thijs Terlouw <thijsterlouw@gmail.com>

--- include/zmq.hpp
+++ include/zmq.hpp
@@ -184,6 +184,14 @@ namespace zmq
             assert (rc == 0);
         }
 
+        //  Be careful with this, it's probably only useful for
+        //  using the C api together with an existing C++ api.
+        //  Normally you should never need to use this.
+        inline operator void* ()
+        {
+            return ptr;
+        }
+        
     private:
 
         void *ptr;
