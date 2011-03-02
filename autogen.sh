18b9ebea32fbf4a4552b0435c8759e5e5c6fe219
Martin Sustrik <sustrik@250bpm.com>
2011-03-02 16:30:40 +0100

    The copyrights in file headers updated.
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-#   Copyright (c) 2007-2010 iMatix Corporation
+
+#    Copyright (c) 2007-2011 iMatix Corporation
+#    Copyright (c) 2007-2011 Other contributors as noted in the AUTHORS file
 #
 #   This file is part of 0MQ.
 #
@@ -7,12 +9,12 @@
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 3 of the License, or
 #   (at your option) any later version.
-
+#
 #   0MQ is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
-
+#
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- src/tcp_connecter.hpp
+++ src/tcp_connecter.hpp
@@ -1,5 +1,6 @@
 /*
-    Copyright (c) 2007-2010 iMatix Corporation
+    Copyright (c) 2007-2011 iMatix Corporation
+    Copyright (c) 2007-2011 Other contributors as noted in the AUTHORS file
 
     This file is part of 0MQ.
 
