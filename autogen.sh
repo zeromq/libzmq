1526ff638c9cb37b156fd80a40889ac3d8258343
Martin Sustrik <sustrik@250bpm.com>
2011-07-04 09:27:50 +0200

    License text in autogen.sh fixed
    autogen.sh incorrectly referred to the license as GPL.
    Changed to LGPL.
    
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- autogen.sh
+++ autogen.sh
@@ -5,17 +5,17 @@
 #
 #   This file is part of 0MQ.
 #
-#   0MQ is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
+#   0MQ is free software; you can redistribute it and/or modify it under
+#   the terms of the GNU Lesser General Public License as published by
 #   the Free Software Foundation; either version 3 of the License, or
 #   (at your option) any later version.
 #
 #   0MQ is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+#   GNU Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public License
+#   You should have received a copy of the GNU Lesser General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Script to generate all required files from fresh git checkout.
