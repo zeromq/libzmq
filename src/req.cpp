8f8bfcaba0f0e827a2d034c76c5b498bbdf4e7e1
Mikko Koppanen <mkoppanen@php.net>
2011-09-13 16:27:07 +0200

    Fixed issue with req assertions (issue 252)
    Signed-off-by: Mikko Koppanen <mkoppanen@php.net>
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/req.cpp
+++ src/req.cpp
@@ -89,8 +89,13 @@ int zmq::req_t::xrecv (msg_t *msg_, int flags_)
         int rc = xreq_t::xrecv (msg_, flags_);
         if (rc != 0)
             return rc;
-        zmq_assert (msg_->flags () & msg_t::label);
-        zmq_assert (msg_->size () == 4);
+
+        // TODO: This should also close the connection with the peer!
+        if (unlikely (!(msg_->flags () & msg_t::label) || msg_->size () != 4)) {
+            errno = EAGAIN;
+            return -1;
+        }
+        
         unsigned char *data = (unsigned char*) msg_->data ();
         if (unlikely (get_uint32 (data) != request_id)) {
 
---
+++ tests/test_invalid_rep.cpp
@@ -0,0 +1,85 @@
+/*
+    Copyright (c) 2007-2011 iMatix Corporation
+    Copyright (c) 2007-2011 Other contributors as noted in the AUTHORS file
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../include/zmq.h"
+#include <assert.h>
+
+int main (int argc, char *argv [])
+{
+    //  Create REQ/XREP wiring.
+    void *ctx = zmq_init (1);
+    assert (ctx);
+    void *xrep_socket = zmq_socket (ctx, ZMQ_XREP);
+    assert (xrep_socket);
+    void *req_socket = zmq_socket (ctx, ZMQ_REQ);
+    assert (req_socket);
+    int linger = 0;
+    int rc = zmq_setsockopt (xrep_socket, ZMQ_LINGER, &linger, sizeof (int));
+    assert (rc == 0);
+    rc = zmq_setsockopt (req_socket, ZMQ_LINGER, &linger, sizeof (int));
+    assert (rc == 0);
+    rc = zmq_bind (xrep_socket, "inproc://hi");
+    assert (rc == 0);
+    rc = zmq_connect (req_socket, "inproc://hi");
+    assert (rc == 0);
+
+    //  Initial request.
+    rc = zmq_send (req_socket, "r", 1, 0);
+    assert (rc == 1);
+
+    //  Receive the request.
+    char addr [4];
+    char seqn [4];
+    char body [1];
+    rc = zmq_recv (xrep_socket, addr, sizeof (addr), 0);
+    assert (rc == 4);
+    rc = zmq_recv (xrep_socket, seqn, sizeof (seqn), 0);
+    assert (rc == 4);
+    rc = zmq_recv (xrep_socket, body, sizeof (body), 0);
+    assert (rc == 1);
+
+    //  Send invalid reply.
+    rc = zmq_send (xrep_socket, addr, 4, 0);
+    assert (rc == 4);
+
+    //  Send valid reply.
+    rc = zmq_send (xrep_socket, addr, 4, ZMQ_SNDLABEL);
+    assert (rc == 4);
+    rc = zmq_send (xrep_socket, seqn, 4, ZMQ_SNDLABEL);
+    assert (rc == 4);
+    rc = zmq_send (xrep_socket, "b", 1, 0);
+    assert (rc == 1);
+
+    //  Check whether we've got the valid reply.
+    rc = zmq_recv (req_socket, body, sizeof (body), 0);
+    assert (rc == 1);
+	assert (body [0] == 'b');
+
+    //  Tear down the wiring.
+    rc = zmq_close (xrep_socket);
+    assert (rc == 0);
+    rc = zmq_close (req_socket);
+    assert (rc == 0);
+    rc = zmq_term (ctx);
+    assert (rc == 0);
+
+    return 0;
+}
+
