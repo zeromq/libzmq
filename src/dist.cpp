cf499ee016340a8534e24084a481a02498b44e70
Martin Sustrik <sustrik@250bpm.com>
2011-09-14 14:31:09 +0200

    Bug in message distribution algorithm fixed (issue 251)
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/dist.cpp
+++ src/dist.cpp
@@ -142,11 +142,9 @@ void zmq::dist_t::distribute (msg_t *msg_, int flags_)
     msg_->add_refs ((int) matching - 1);
 
     //  Push copy of the message to each matching pipe.
-    for (pipes_t::size_type i = 0; i < matching;) {
+    for (pipes_t::size_type i = 0; i < matching; ++i) {
         if (!write (pipes [i], msg_))
             msg_->rm_refs (1);
-        else
-            i++;
     }
 
     //  Detach the original message from the data buffer. Note that we don't
