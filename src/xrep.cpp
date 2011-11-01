8e21d64c974344b5b2b83cac85d12c51392fe74b
Martin Sustrik <sustrik@250bpm.com>
2011-11-01 18:06:11 +0100

    Copyright dates adjusted to reflect reality
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/xrep.cpp
+++ src/xrep.cpp
@@ -1,6 +1,6 @@
 /*
     Copyright (c) 2009-2011 250bpm s.r.o.
-    Copyright (c) 2007-2011 iMatix Corporation
+    Copyright (c) 2011 iMatix Corporation
     Copyright (c) 2011 VMware, Inc.
     Copyright (c) 2007-2011 Other contributors as noted in the AUTHORS file
 
