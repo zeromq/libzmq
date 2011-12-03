aaac96d94a81fb5debe24200e20c353217baec9c
Mika Fischer <mika.fischer@zoopnet.de>
2011-12-03 13:07:30 +0100

    This makes clock_t insensitive to the system clock being reset by NTP or the sysadmin, which could previously cause long hangs for instance in zmq_poll.
    Signed-off-by: Mika Fischer <mika.fischer@zoopnet.de>

--- src/clock.cpp
+++ src/clock.cpp
@@ -34,6 +34,10 @@
 #include <sys/time.h>
 #endif
 
+#if defined HAVE_CLOCK_GETTIME
+#include <time.h>
+#endif
+
 zmq::clock_t::clock_t () :
     last_tsc (rdtsc ()),
     last_time (now_us () / 1000)
@@ -60,6 +64,14 @@ uint64_t zmq::clock_t::now_us ()
     //  since the system was started.
     double ticks_div = (double) (ticksPerSecond.QuadPart / 1000000);     
     return (uint64_t) (tick.QuadPart / ticks_div);
+
+#elif defined HAVE_CLOCK_GETTIME
+
+    //  Use POSIX clock_gettime function to get precise monotonic time.
+    struct timespec tv;
+    int rc = clock_gettime (CLOCK_MONOTONIC, &tv);
+    errno_assert (rc == 0);
+    return (tv.tv_sec * (uint64_t) 1000000 + tv.tv_nsec / 1000);
 
 #else
 
