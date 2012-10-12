56bb3df1f6d1d44b1516cfe9884a41d929a1f1fb
Steve-o <fnjordy@gmail.com>
2012-10-12 15:36:34 +0000

    Detect ZeroMQ version at build time; hide NSIS dependencies in new cmake folder.

---
+++ cmake/Modules/TestZMQVersion.cmake
@@ -0,0 +1,27 @@
+
+  MESSAGE(STATUS "Detecting ZMQ")
+        SET(TRY_RUN_DIR ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/zmq_run.dir)
+
+        TRY_RUN(RUN_RESULT COMPILE_RESULT
+          ${TRY_RUN_DIR}
+          ${CMAKE_SOURCE_DIR}/cmake/Modules/zmq_version.cpp
+          CMAKE_FLAGS 
+            "-DINCLUDE_DIRECTORIES:STRING=${CMAKE_SOURCE_DIR}/include"
+          COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT
+          RUN_OUTPUT_VARIABLE RUN_OUTPUT)
+
+        IF(COMPILE_RESULT)
+          IF(RUN_RESULT MATCHES "FAILED_TO_RUN")
+            MESSAGE(STATUS "Detecting ZMQ - failed")
+          ELSE()
+            STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\1" ZMQ_VERSION_MAJOR "${RUN_OUTPUT}")
+            STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\2" ZMQ_VERSION_MINOR "${RUN_OUTPUT}")
+            STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\3" ZMQ_VERSION_PATCH "${RUN_OUTPUT}")
+            MESSAGE(STATUS "Detecting ZMQ - ${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}.${ZMQ_VERSION_PATCH}")
+          ENDIF()
+        ELSE()
+          MESSAGE(STATUS "Check for ZMQ version - not found")
+          MESSAGE(STATUS "Detecting ZMQ - failed")
+        ENDIF()
+
+
