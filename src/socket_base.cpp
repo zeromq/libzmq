2388f27bfe334d9a98f417bb0c87cfea5727fce6
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-12-04 13:14:56 +0100

    Close inproc socket pairs on zmq_disconnect
    This patch fixes LIBZMQ-476 and LIBZMQ-475

--- src/socket_base.cpp
+++ src/socket_base.cpp
@@ -478,6 +478,10 @@ int zmq::socket_base_t::connect (const char *addr_)
         // Save last endpoint URI
         options.last_endpoint.assign (addr_);
 
+        // remember inproc connections for disconnect
+        inprocs.insert (inprocs_t::value_type (std::string (addr_), pipes[0]));
+        inprocs.insert (inprocs_t::value_type (std::string (addr_), pipes[1]));
+
         return 0;
     }
 
@@ -583,6 +587,30 @@ int zmq::socket_base_t::term_endpoint (const char *addr_)
     int rc = process_commands (0, false);
     if (unlikely (rc != 0))
         return -1;
+
+    //  Parse addr_ string.
+    std::string protocol;
+    std::string address;
+    rc = parse_uri (addr_, protocol, address);
+    if (rc != 0)
+        return -1;
+
+    rc = check_protocol (protocol);
+    if (rc != 0)
+        return -1;
+
+    // Disconnect an inproc socket
+    if (protocol == "inproc") {
+        std::pair <inprocs_t::iterator, inprocs_t::iterator> range = inprocs.equal_range (std::string (addr_));
+        if (range.first == range.second)
+            return -1;
+	
+        for (inprocs_t::iterator it = range.first; it != range.second; ++it)
+            it->second->terminate(true);
+        inprocs.erase (range.first, range.second);
+        return 0;
+    }
+
 
     //  Find the endpoints range (if any) corresponding to the addr_ string.
     std::pair <endpoints_t::iterator, endpoints_t::iterator> range = endpoints.equal_range (std::string (addr_));
