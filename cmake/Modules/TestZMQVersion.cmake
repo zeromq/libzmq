74763e2c8ee5eccf2bea0ca64e7f784be4307404
Matt Arsenault <arsenm2@rpi.edu>
2013-01-01 05:34:44 -0500

    Don't use the toplevel cmake paths so the build works as a subproject

--- cmake/Modules/TestZMQVersion.cmake
+++ cmake/Modules/TestZMQVersion.cmake
@@ -1,35 +1,18 @@
 
-  MESSAGE(STATUS "Detecting ZMQ")
-        SET(TRY_RUN_DIR ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/zmq_run.dir)
+file(READ "${PROJECT_SOURCE_DIR}/include/zmq.h" _ZMQ_H_CONTENTS)
+string(REGEX REPLACE ".*#define ZMQ_VERSION_MAJOR ([0-9]+).*" "\\1" ZMQ_VERSION_MAJOR "${_ZMQ_H_CONTENTS}")
+string(REGEX REPLACE ".*#define ZMQ_VERSION_MINOR ([0-9]+).*" "\\1" ZMQ_VERSION_MINOR "${_ZMQ_H_CONTENTS}")
+string(REGEX REPLACE ".*#define ZMQ_VERSION_PATCH ([0-9]+).*" "\\1" ZMQ_VERSION_PATCH "${_ZMQ_H_CONTENTS}")
+set(ZMQ_VERSION "${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}.${ZMQ_VERSION_PATCH}")
 
-        TRY_RUN(RUN_RESULT COMPILE_RESULT
-          ${TRY_RUN_DIR}
-          ${CMAKE_SOURCE_DIR}/cmake/Modules/zmq_version.cpp
-          CMAKE_FLAGS
-            "-DINCLUDE_DIRECTORIES:STRING=${CMAKE_SOURCE_DIR}/include"
-          COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT
-          RUN_OUTPUT_VARIABLE RUN_OUTPUT)
+message(STATUS "Detected ZMQ Version - ${ZMQ_VERSION}")
 
-        IF(COMPILE_RESULT)
-          IF(RUN_RESULT MATCHES "FAILED_TO_RUN")
-            MESSAGE(STATUS "Detecting ZMQ - failed")
-          ELSE()
-            STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\1" ZMQ_VERSION_MAJOR "${RUN_OUTPUT}")
-            STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\2" ZMQ_VERSION_MINOR "${RUN_OUTPUT}")
-            STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\3" ZMQ_VERSION_PATCH "${RUN_OUTPUT}")
-            MESSAGE(STATUS "Detecting ZMQ - ${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}.${ZMQ_VERSION_PATCH}")
-          ENDIF()
-        ELSE()
-          MESSAGE(STATUS "Check for ZMQ version - not found")
-          MESSAGE(STATUS "Detecting ZMQ - failed")
-        ENDIF()
-
-  if(MSVC_VERSION MATCHES "1700")
-    set(_zmq_COMPILER "-v110")
-  elseif(MSVC10)
-    set(_zmq_COMPILER "-v100")
-  elseif(MSVC90)
-    set(_zmq_COMPILER "-v90")
-  else()
-    set(_zmq_COMPILER "")
-  endif()
+if(MSVC_VERSION MATCHES "1700")
+  set(_zmq_COMPILER "-v110")
+elseif(MSVC10)
+  set(_zmq_COMPILER "-v100")
+elseif(MSVC90)
+  set(_zmq_COMPILER "-v90")
+else()
+  set(_zmq_COMPILER "")
+endif()
