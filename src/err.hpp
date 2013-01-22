ba3e18f700659202247e38a7f48ea16d32bd8e79
Charles McGarvey <chazmcgarvey@brokenzipper.com>
2013-01-21 17:14:26 -0700

    define a dummy EPROTO for platforms that don't use it
    This accomplishes the same thing as 2e2ef7fd in a potentially cleaner way.

--- src/err.hpp
+++ src/err.hpp
@@ -44,6 +44,11 @@
 #include <netdb.h>
 #endif
 
+// EPROTO is not used by OpenBSD and maybe other platforms.
+#ifndef EPROTO
+#define EPROTO 0
+#endif
+
 namespace zmq
 {
     const char *errno_to_string (int errno_);
