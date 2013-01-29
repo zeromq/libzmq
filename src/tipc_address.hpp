dbc42ee9af098183714845a75139997dcc34c7a0
Erik Hugne <erik.hugne@ericsson.com>
2013-01-29 14:16:38 +0100

    zmq: add support for TIPC transport
    A ZeroMQ application can opt for TIPC based sockets
    using the TIPC port name format:
    zmq_bind(sb, "tipc://{type,lower,upper}");
    zmq_connect(sc, "tipc://{type,inst}");
    
    'type' is the service ID, and 'lower/upper' can be
    used for service partitioning or basic load
    balancing.
    
    ZeroMQ TIPC transport requires a kernel >= 3.8
    (nonblocking connect support for TIPC).
    
    Signed-off-by: Erik Hugne <erik.hugne@ericsson.com>

---
+++ src/tipc_address.hpp
@@ -0,0 +1,67 @@
+/*
+    Copyright (c) 2011 250bpm s.r.o.
+    Copyright (c) 2011 Other contributors as noted in the AUTHORS file
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __ZMQ_TIPC_ADDRESS_HPP_INCLUDED__
+#define __ZMQ_TIPC_ADDRESS_HPP_INCLUDED__
+
+#include <string>
+
+#include "platform.hpp"
+
+#if defined ZMQ_HAVE_LINUX
+
+#include <sys/socket.h>
+#include <linux/tipc.h>
+
+namespace zmq
+{
+
+    class tipc_address_t
+    {
+    public:
+
+        tipc_address_t ();
+        tipc_address_t (const sockaddr *sa, socklen_t sa_len);
+        ~tipc_address_t ();
+
+        //  This function sets up the address "{type, lower, upper}" for TIPC transport
+        int resolve (const char* name);
+
+        //  The opposite to resolve()
+        int to_string (std::string &addr_);
+
+        const sockaddr *addr () const;
+        socklen_t addrlen () const;
+
+    private:
+
+        struct sockaddr_tipc address;
+
+        tipc_address_t (const tipc_address_t&);
+        const tipc_address_t &operator = (const tipc_address_t&);
+    };
+    
+}
+
+#endif
+
+#endif
+
+
