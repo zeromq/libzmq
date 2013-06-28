70417701089e5918452996d7cbe48c89e35a39e0
Pieter Hintjens <ph@imatix.com>
2013-06-28 22:10:22 +0200

    Added Z85 support
    The use of binary for CURVE keys is painful; you cannot easily copy
    these in e.g. email, or use them directly in source code. There are
    various encoding possibilities. Base16 and Base64 are not optimal.
    Ascii85 is not safe for source (it generates quotes and escapes).
    
    So, I've designed a new Base85 encoding, Z85, which is safe to use
    in code and elsewhere, and I've modified libzmq to use this where
    it also uses binary keys (in get/setsockopt).
    
    Very simply, if you use a 32-byte value, it's Base256 (binary),
    and if you use a 40-byte value, it's Base85 (Z85).
    
    I've put the Z85 codec into z85_codec.hpp, it's not elegant C++
    but it is minimal and it works. Feel free to rewrap as a real class
    if this annoys you.

--- tests/test_security_curve.cpp
+++ tests/test_security_curve.cpp
@@ -56,7 +56,7 @@ zap_handler (void *zap)
 int main (void)
 {
 #ifndef HAVE_LIBSODIUM
-    printf("libsodium not installed, skipping CURVE test\n");
+    printf ("libsodium not installed, skipping CURVE test\n");
     return 0;
 #endif
     int rc;
@@ -75,43 +75,24 @@ int main (void)
     void *client = zmq_socket (ctx, ZMQ_DEALER);
     assert (client);
 
+    //  Test keys from the zmq_curve man page
+    char client_public [] = "Yne@$w-vo<fVvi]a<NY6T1ed:M$fCG*[IaLV{hID";
+    char client_secret [] = "D:)Q[IlAW!ahhC2ac:9*A}h:p?([4%wOTJ%JR%cs";
+    char server_public [] = "rq:rM>}U?@Lns47E1%kR.o@n%FcmmsL/@{H8]yf7";
+    char server_secret [] = "JTKVSB%%)wK0E.X)V>+}o?pNmC{O&4W4b!Ni{Lh6";
+        
     as_server = 1;
-    rc = zmq_setsockopt(server, ZMQ_CURVE_SERVER, &as_server, sizeof(int));
-    // Test key from the zmq_curve man page.
-    uint8_t server_secret[32] = {
-      0x8E, 0x0B, 0xDD, 0x69, 0x76, 0x28, 0xB9, 0x1D, 0x8F, 0x24,
-      0x55, 0x87, 0xEE, 0x95, 0xC5, 0xB0, 0x4D, 0x48, 0x96, 0x3F,
-      0x79, 0x25, 0x98, 0x77, 0xB4, 0x9C, 0xD9, 0x06, 0x3A, 0xEA, 
-      0xD3, 0xB7  
-    };
-    rc = zmq_setsockopt(server, ZMQ_CURVE_SECRETKEY, server_secret, sizeof(server_secret));
-    assert(rc == 0);
+    rc = zmq_setsockopt (server, ZMQ_CURVE_SERVER, &as_server, sizeof (int));
+    assert (rc == 0);
+    rc = zmq_setsockopt (server, ZMQ_CURVE_SECRETKEY, server_secret, 40);
+    assert (rc == 0);
 
-    // Test client keys from the zmq_curve man page.
-    uint8_t server_public[32] = {
-        0x54, 0xFC, 0xBA, 0x24, 0xE9, 0x32, 0x49, 0x96, 0x93, 0x16,
-        0xFB, 0x61, 0x7C, 0x87, 0x2B, 0xB0, 0xC1, 0xD1, 0xFF, 0x14,
-        0x80, 0x04, 0x27, 0xC5, 0x94, 0xCB, 0xFA, 0xCF, 0x1B, 0xC2,
-        0xD6, 0x52
-    };
-    rc = zmq_setsockopt(client, ZMQ_CURVE_SERVERKEY, server_public, sizeof(server_public));
-    assert(rc == 0);
-    uint8_t client_public[32] = {
-        0xBB, 0x88, 0x47, 0x1D, 0x65, 0xE2, 0x65, 0x9B, 0x30, 0xC5,
-        0x5A, 0x53, 0x21, 0xCE, 0xBB, 0x5A, 0xAB, 0x2B, 0x70, 0xA3,
-        0x98, 0x64, 0x5C, 0x26, 0xDC, 0xA2, 0xB2, 0xFC, 0xB4, 0x3F,
-        0xC5, 0x18
-    };
-    rc = zmq_setsockopt(client, ZMQ_CURVE_PUBLICKEY, client_public, sizeof(client_public));
-    assert(rc == 0);
-    uint8_t client_secret[32] = {
-        0x7B, 0xB8, 0x64, 0xB4, 0x89, 0xAF, 0xA3, 0x67, 0x1F, 0xBE,
-        0x69, 0x10, 0x1F, 0x94, 0xB3, 0x89, 0x72, 0xF2, 0x48, 0x16,
-        0xDF, 0xB0, 0x1B, 0x51, 0x65, 0x6B, 0x3F, 0xEC, 0x8D, 0xFD,
-        0x08, 0x88
-    };
-    rc = zmq_setsockopt(client, ZMQ_CURVE_SECRETKEY, client_secret, sizeof(client_secret));
-    assert(rc == 0);
+    rc = zmq_setsockopt (client, ZMQ_CURVE_SERVERKEY, server_public, 40);
+    assert (rc == 0);
+    rc = zmq_setsockopt (client, ZMQ_CURVE_PUBLICKEY, client_public, 40);
+    assert (rc == 0);
+    rc = zmq_setsockopt (client, ZMQ_CURVE_SECRETKEY, client_secret, 40);
+    assert (rc == 0);
 
     // Test the client and server both have the right mechanism.
     optsize = sizeof (int);
