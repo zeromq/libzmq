62f3fdf3b70fccff393fa9f5634faddf93a5ec5a
Pieter Hintjens <ph@imatix.com>
2013-12-20 14:17:35 +0100

    Fixed indentation

--- src/ipc_address.cpp
+++ src/ipc_address.cpp
@@ -53,8 +53,8 @@ int zmq::ipc_address_t::resolve (const char *path_)
     }
 #if defined ZMQ_HAVE_LINUX
     if (path_[0] == '@' && !path_[1]) {
-            errno = EINVAL;
-            return -1;
+        errno = EINVAL;
+        return -1;
     }
 #endif
 
