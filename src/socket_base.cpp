e6ef16d005a8ad2c5cf2626654b5576042329d9c
Alex Pyrgiotis <apyrgio@grnet.gr>
2014-02-03 17:35:53 +0200

    Fix potential segmentation fault
    Call the allocation assertion macro before dereferencing the socket
    pointer.

--- src/socket_base.cpp
+++ src/socket_base.cpp
@@ -121,10 +121,11 @@ zmq::socket_base_t *zmq::socket_base_t::create (int type_, class ctx_t *parent_,
             errno = EINVAL;
             return NULL;
     }
+
+    alloc_assert (s);
     if (s->mailbox.get_fd () == retired_fd)
         return NULL;
 
-    alloc_assert (s);
     return s;
 }
 
