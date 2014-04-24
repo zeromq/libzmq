d1334d9df899f56bc75e84e8ee797fe888620b2a
Chris Busbey <chris.busbey@gmail.com>
2014-04-24 14:39:29 -0700

    using latest copyright header

--- src/gssapi_server.cpp
+++ src/gssapi_server.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2007-2013 Contributors as noted in the AUTHORS file
+    Copyright (c) 2007-2014 Contributors as noted in the AUTHORS file
 
     This file is part of 0MQ.
 
@@ -16,7 +16,6 @@
     You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #include "platform.hpp"
 #ifdef ZMQ_HAVE_WINDOWS
