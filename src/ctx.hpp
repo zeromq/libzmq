54e0fde1ccf37e5bb1591bf66e8ca01e8074a155
Martin Hurton <hurtonm@gmail.com>
2014-07-09 09:57:28 +0200

    Resolve issue #949

--- src/ctx.hpp
+++ src/ctx.hpp
@@ -104,6 +104,7 @@ namespace zmq
 
         //  Management of inproc endpoints.
         int register_endpoint (const char *addr_, const endpoint_t &endpoint_);
+        int unregister_endpoint (const std::string &addr_, socket_base_t *socket_);
         void unregister_endpoints (zmq::socket_base_t *socket_);
         endpoint_t find_endpoint (const char *addr_);
         void pend_connection (const std::string &addr_,
