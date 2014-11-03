af870d2e0b4d9260cd83d93264a48f3c6aab5ca1
Joe McIlvain <joe.eli.mac@gmail.com>
2014-11-02 17:17:51 -0800

    foo

--- builds/qt-android/ci_build.sh
+++ builds/qt-android/ci_build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export ANDROID_NDK_ROOT="/android-ndk" && \
-export TOOLCHAIN_PATH="/android-ndk/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin" && \
-export TOOLCHAIN_NAME="arm-linux-androideabi-4.8" && \
-export TOOLCHAIN_HOST="arm-linux-androideabi" && \
-export TOOLCHAIN_ARCH="arm" && \
-./build.sh
+export ANDROID_NDK_ROOT="/android-ndk"
+export TOOLCHAIN_PATH="/android-ndk/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin"
+export TOOLCHAIN_NAME="arm-linux-androideabi-4.8"
+export TOOLCHAIN_HOST="arm-linux-androideabi"
+export TOOLCHAIN_ARCH="arm"
+
+source ./build.sh
