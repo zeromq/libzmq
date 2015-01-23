zeromq/libzmq

commit afb24b53e6d1fc39cd1b73be706b335ed5c7b6fb

Add STREAM connect notification. Adjust test cases to connection notification. Increase error checking in test cases.

src/stream_engine.cpp
@@ -177,6 +177,14 @@ void zmq::stream_engine_t::plug (io_thread_t *io_thread_,
 
         read_msg = &stream_engine_t::pull_msg_from_session;
         write_msg = &stream_engine_t::push_msg_to_session;
+
+        //  For raw sockets, send an initial 0-length message to the
+        // application so that it knows a peer has connected.
+        msg_t connector;
+        connector.init();
+        int rc = (this->*write_msg) (&connector);
+        connector.close();
+        session->flush ();
     }
     else {
         //  Send the 'length' and 'flags' fields of the identity message.

tests/test_stream.cpp
@@ -68,6 +68,7 @@ test_stream_to_dealer (void)
     rc = zmq_send (dealer, "Hello", 5, 0);
     assert (rc == 5);
 
+    //  Connecting sends a zero message
     //  First frame is identity
     zmq_msg_t identity;
     rc = zmq_msg_init (&identity);
@@ -76,8 +77,18 @@ test_stream_to_dealer (void)
     assert (rc > 0);
     assert (zmq_msg_more (&identity));
 
-    //  Second frame is greeting signature
+    //  Second frame is zero
     byte buffer [255];
+    rc = zmq_recv (stream, buffer, 255, 0);
+    assert (rc == 0);
+    
+    //  Real data follows
+    //  First frame is identity
+    rc = zmq_msg_recv (&identity, stream, 0);
+    assert (rc > 0);
+    assert (zmq_msg_more (&identity));
+
+    //  Second frame is greeting signature
     rc = zmq_recv (stream, buffer, 255, 0);
     assert (rc == 10);
     assert (memcmp (buffer, greeting.signature, 10) == 0);
@@ -178,14 +189,26 @@ test_stream_to_stream (void)
     assert (client);
     rc = zmq_connect (client, "tcp://localhost:9070");
     assert (rc == 0);
-    //  It would be less surprising to get an empty message instead
-    //  of having to fetch the identity like this [PH 2013/06/27]
     uint8_t id [256];
     size_t id_size = 256;
+    uint8_t buffer [256];
+    
+    //  Connecting sends a zero message
+    //  Server: First frame is identity, second frame is zero
+    id_size = zmq_recv (server, id, 256, 0);
+    assert (id_size > 0);
+    rc = zmq_recv (server, buffer, 256, 0);
+    assert (rc == 0);
+    //  Client: First frame is identity, second frame is zero
+    id_size = zmq_recv (client, id, 256, 0);
+    assert (id_size > 0);
+    rc = zmq_recv (client, buffer, 256, 0);
+    assert (rc == 0);
+
+    //  Sent HTTP request on client socket
+    //  Get server identity
     rc = zmq_getsockopt (client, ZMQ_IDENTITY, id, &id_size);
     assert (rc == 0);
-    
-    //  Sent HTTP request on client socket
     //  First frame is server identity
     rc = zmq_send (client, id, id_size, ZMQ_SNDMORE);
     assert (rc == (int) id_size);
@@ -196,9 +219,8 @@ test_stream_to_stream (void)
     //  Get HTTP request; ID frame and then request
     id_size = zmq_recv (server, id, 256, 0);
     assert (id_size > 0);
-    uint8_t buffer [256];
     rc = zmq_recv (server, buffer, 256, 0);
-    assert (rc > 0);
+    assert (rc != -1);
     assert (memcmp (buffer, "GET /\n\n", 7) == 0);
     
     //  Send reply back to client
@@ -207,8 +229,16 @@ test_stream_to_stream (void)
         "Content-Type: text/plain\r\n" 
         "\r\n" 
         "Hello, World!";
-    zmq_send (server, id, id_size, ZMQ_SNDMORE);
-    zmq_send (server, http_response, sizeof (http_response), 0);
+    rc = zmq_send (server, id, id_size, ZMQ_SNDMORE);
+    assert (rc != -1);
+    rc = zmq_send (server, http_response, sizeof (http_response), ZMQ_SNDMORE);
+    assert (rc != -1);
+
+    //  Send zero to close connection to client
+    rc = zmq_send (server, id, id_size, ZMQ_SNDMORE);
+    assert (rc != -1);
+    rc = zmq_send (server, NULL, 0, ZMQ_SNDMORE);
+    assert (rc != -1);
 
     //  Get reply at client and check that it's complete
     id_size = zmq_recv (client, id, 256, 0);
@@ -216,6 +246,22 @@ test_stream_to_stream (void)
     rc = zmq_recv (client, buffer, 256, 0);
     assert (rc == sizeof (http_response));
     assert (memcmp (buffer, http_response, sizeof (http_response)) == 0);
+
+    // //  Get disconnection notification
+    // FIXME: why does this block? Bug in STREAM disconnect notification?
+    // id_size = zmq_recv (client, id, 256, 0);
+    // assert (id_size > 0);
+    // rc = zmq_recv (client, buffer, 256, 0);
+    // assert (rc == 0);
+
+    rc = zmq_close (server);
+    assert (rc == 0);
+
+    rc = zmq_close (client);
+    assert (rc == 0);
+
+    rc = zmq_ctx_term (ctx);
+    assert (rc == 0);
 }
 
 

tests/test_stream_disconnect_notifications.cpp
@@ -68,22 +68,60 @@ int main(int argc, char** argv)
     rc = zmq_connect (sockets [CLIENT], "tcp://localhost:6666");
     assert (rc == 0);
 
-    // TODO: wait for client to become ready.
+    // wait for connect notification
+    // Server: Grab the 1st frame (peer identity).
+    zmq_msg_t peer_frame;
+    rc = zmq_msg_init (&peer_frame);
+    assert (rc == 0);
+    rc = zmq_msg_recv (&peer_frame, sockets [SERVER], 0);
+    assert (rc != -1);
+    assert(zmq_msg_size (&peer_frame) > 0);
+    assert (has_more (sockets [SERVER]));
+
+    // Server: Grab the 2nd frame (actual payload).
+    zmq_msg_t data_frame;
+    rc = zmq_msg_init (&data_frame);
+    assert (rc == 0);
+    rc = zmq_msg_recv (&data_frame, sockets [SERVER], 0);
+    assert (rc != -1);
+    assert(zmq_msg_size (&data_frame) == 0);
+
+    // Client: Grab the 1st frame (peer identity).
+    rc = zmq_msg_init (&peer_frame);
+    assert (rc == 0);
+    rc = zmq_msg_recv (&peer_frame, sockets [CLIENT], 0);
+    assert (rc != -1);
+    assert(zmq_msg_size (&peer_frame) > 0);
+    assert (has_more (sockets [CLIENT]));
+
+    // Client: Grab the 2nd frame (actual payload).
+    rc = zmq_msg_init (&data_frame);
+    assert (rc == 0);
+    rc = zmq_msg_recv (&data_frame, sockets [CLIENT], 0);
+    assert (rc != -1);
+    assert(zmq_msg_size (&data_frame) == 0);
 
     // Send initial message.
     char blob_data [256];
     size_t blob_size = sizeof(blob_data);
     rc = zmq_getsockopt (sockets [CLIENT], ZMQ_IDENTITY, blob_data, &blob_size);
-    assert (rc == 0);
+    assert (rc != -1);
+    assert(blob_size > 0);
     zmq_msg_t msg;
-    zmq_msg_init_size (&msg, blob_size);
+    rc = zmq_msg_init_size (&msg, blob_size);
+    assert (rc == 0);
     memcpy (zmq_msg_data (&msg), blob_data, blob_size);
-    zmq_msg_send (&msg, sockets [dialog [0].turn], ZMQ_SNDMORE);
-    zmq_msg_close (&msg);
-    zmq_msg_init_size (&msg, strlen(dialog [0].text)+1);
-    memcpy (zmq_msg_data (&msg), dialog [0].text, strlen(dialog [0].text)+1);
-    zmq_msg_send (&msg, sockets [dialog [0].turn], ZMQ_SNDMORE);
-    zmq_msg_close (&msg);
+    rc = zmq_msg_send (&msg, sockets [dialog [0].turn], ZMQ_SNDMORE);
+    assert (rc != -1);
+    rc = zmq_msg_close (&msg);
+    assert (rc == 0);
+    rc = zmq_msg_init_size (&msg, strlen(dialog [0].text));
+    assert (rc == 0);
+    memcpy (zmq_msg_data (&msg), dialog [0].text, strlen(dialog [0].text));
+    rc = zmq_msg_send (&msg, sockets [dialog [0].turn], ZMQ_SNDMORE);
+    assert (rc != -1);
+    rc = zmq_msg_close (&msg);
+    assert (rc == 0);
 
     // TODO: make sure this loop doesn't loop forever if something is wrong
     //       with the test (or the implementation).
@@ -106,47 +144,64 @@ int main(int argc, char** argv)
 
             // Grab the 1st frame (peer identity).
             zmq_msg_t peer_frame;
-            zmq_msg_init (&peer_frame);
-            zmq_msg_recv (&peer_frame, sockets [SERVER], 0);
+            rc = zmq_msg_init (&peer_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_recv (&peer_frame, sockets [SERVER], 0);
+	    assert (rc != -1);
+	    assert(zmq_msg_size (&peer_frame) > 0);
             assert (has_more (sockets [SERVER]));
 
             // Grab the 2nd frame (actual payload).
             zmq_msg_t data_frame;
-            zmq_msg_init (&data_frame);
-            zmq_msg_recv (&data_frame, sockets [SERVER], 0);
+            rc = zmq_msg_init (&data_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_recv (&data_frame, sockets [SERVER], 0);
+	    assert (rc != -1);
 
             // Make sure payload matches what we expect.
             const char * const data = (const char*)zmq_msg_data (&data_frame);
             const int size = zmq_msg_size (&data_frame);
-            int cmp = memcmp(dialog [step].text, data, size);
-            assert (cmp == 0);
-
-            ++step;
-
             // 0-length frame is a disconnection notification.  The server
             // should receive it as the last step in the dialogue.
             if (size == 0) {
                 printf ("server received disconnection notification!\n");
+		++step;
                 assert (step == steps);
             }
             else {
                 printf ("server received %d bytes.\n", size);
+		fprintf(stderr, "size = %d, len = %ld\n", size, strlen(dialog [step].text));
+		assert((size_t)size == strlen(dialog [step].text));
+		int cmp = memcmp(dialog [step].text, data, size);
+		assert (cmp == 0);
+
+		++step;
+
                 assert (step < steps);
 
                 // Prepare the response.
-                zmq_msg_close (&data_frame);
-                zmq_msg_init_size (&data_frame, strlen (dialog [step].text));
-                memcpy (zmq_msg_data (&data_frame), dialog [step].text, zmq_msg_size (&data_frame));
+                rc = zmq_msg_close (&data_frame);
+		assert (rc == 0);
+                rc = zmq_msg_init_size (&data_frame,
+					strlen (dialog [step].text));
+		assert (rc == 0);
+                memcpy (zmq_msg_data (&data_frame), dialog [step].text,
+			zmq_msg_size (&data_frame));
 
                 // Send the response.
-                printf ("server sending %d bytes.\n", (int)zmq_msg_size (&data_frame));
-                zmq_msg_send (&peer_frame, sockets [SERVER], ZMQ_SNDMORE);
-                zmq_msg_send (&data_frame, sockets [SERVER], ZMQ_SNDMORE);
+                printf ("server sending %d bytes.\n",
+			(int)zmq_msg_size (&data_frame));
+                rc = zmq_msg_send (&peer_frame, sockets [SERVER], ZMQ_SNDMORE);
+		assert (rc != -1);
+                rc = zmq_msg_send (&data_frame, sockets [SERVER], ZMQ_SNDMORE);
+		assert (rc != -1);
             }
 
             // Release resources.
-            zmq_msg_close (&peer_frame);
-            zmq_msg_close (&data_frame);
+            rc = zmq_msg_close (&peer_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_close (&data_frame);
+	    assert (rc == 0);
         }
 
         // Check for data received by the client.
@@ -155,18 +210,26 @@ int main(int argc, char** argv)
 
             // Grab the 1st frame (peer identity).
             zmq_msg_t peer_frame;
-            zmq_msg_init (&peer_frame);
-            zmq_msg_recv (&peer_frame, sockets [CLIENT], 0);
+            rc = zmq_msg_init (&peer_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_recv (&peer_frame, sockets [CLIENT], 0);
+	    assert (rc != -1);
+	    assert(zmq_msg_size (&peer_frame) > 0);
             assert (has_more (sockets [CLIENT]));
 
             // Grab the 2nd frame (actual payload).
             zmq_msg_t data_frame;
-            zmq_msg_init (&data_frame);
-            zmq_msg_recv (&data_frame, sockets [CLIENT], 0);
+            rc = zmq_msg_init (&data_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_recv (&data_frame, sockets [CLIENT], 0);
+	    assert (rc != -1);
+	    assert(zmq_msg_size (&data_frame) > 0);
 
             // Make sure payload matches what we expect.
             const char * const data = (const char*)zmq_msg_data (&data_frame);
             const int size = zmq_msg_size (&data_frame);
+	    fprintf(stderr, "size = %d, len = %ld\n", size, strlen(dialog [step].text));
+	    assert((size_t)size == strlen(dialog [step].text));
             int cmp = memcmp(dialog [step].text, data, size);
             assert (cmp == 0);
 
@@ -176,25 +239,34 @@ int main(int argc, char** argv)
 
             // Prepare the response (next line in the dialog).
             assert (step < steps);
-            zmq_msg_close (&data_frame);
-            zmq_msg_init_size (&data_frame, strlen (dialog [step].text));
+            rc = zmq_msg_close (&data_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_init_size (&data_frame, strlen (dialog [step].text));
+	    assert (rc == 0);
             memcpy (zmq_msg_data (&data_frame), dialog [step].text, zmq_msg_size (&data_frame));
 
             // Send the response.
             printf ("client sending %d bytes.\n", (int)zmq_msg_size (&data_frame));
-            zmq_msg_send (&peer_frame, sockets [CLIENT], ZMQ_SNDMORE);
-            zmq_msg_send (&data_frame, sockets [CLIENT], ZMQ_SNDMORE);
+            rc = zmq_msg_send (&peer_frame, sockets [CLIENT], ZMQ_SNDMORE);
+	    assert (rc != -1);
+            rc = zmq_msg_send (&data_frame, sockets [CLIENT], ZMQ_SNDMORE);
+	    assert (rc != -1);
 
             // Release resources.
-            zmq_msg_close (&peer_frame);
-            zmq_msg_close (&data_frame);
+            rc = zmq_msg_close (&peer_frame);
+	    assert (rc == 0);
+            rc = zmq_msg_close (&data_frame);
+	    assert (rc == 0);
         }
     }
     assert (step == steps);
 
     printf ("Done, exiting now.\n");
-    zmq_close (sockets [CLIENT]);
-    zmq_close (sockets [SERVER]);
-    zmq_ctx_term (context);
+    rc = zmq_close (sockets [CLIENT]);
+    assert (rc == 0);
+    rc = zmq_close (sockets [SERVER]);
+    assert (rc == 0);
+    rc = zmq_ctx_term (context);
+    assert (rc == 0);
     return 0;
 }

commit 6ced7027a0861e22a20bfa0a872b55cade6323ec

Problem: commit afb24b53 broke ZMQ_STREAM contract
Symptom is that ZMQ_STREAM sockets in 4.1.0 and 4.1.1 generate zero
sized messages on each new connection, unlike 4.0.x which did not do
this.

Person who made this commit also changed test cases so that contract
breakage did not show. Same person was later banned for persistently
poor form in CZMQ contributions.

Solution: enable connect notifications on ZMQ_STREAM sockets using a
new ZMQ_STREAM_NOTIFY setting. By default, socket does not deliver
notifications, and behaves as in 4.0.x.

Fixes #1316

include/zmq.h
@@ -297,6 +297,7 @@ ZMQ_EXPORT const char *zmq_msg_gets (zmq_msg_t *msg, const char *property);
 #define ZMQ_BLOCKY 70
 #define ZMQ_XPUB_MANUAL 71
 #define ZMQ_XPUB_WELCOME_MSG 72
+#define ZMQ_STREAM_NOTIFY 73
 
 /*  Message options                                                           */
 #define ZMQ_MORE 1

src/options.cpp
@@ -46,7 +46,8 @@ zmq::options_t::options_t () :
     immediate (0),
     filter (false),
     recv_identity (false),
-    raw_sock (false),
+    raw_socket (false),
+    raw_notify (false),
     tcp_keepalive (-1),
     tcp_keepalive_cnt (-1),
     tcp_keepalive_idle (-1),

src/options.hpp
@@ -112,7 +112,8 @@ namespace zmq
         bool recv_identity;
 
         // if true, router socket accepts non-zmq tcp connections
-        bool raw_sock;
+        bool raw_socket;
+        bool raw_notify;        //  Provide connect notifications
 
         //  Addres of SOCKS proxy
         std::string socks_proxy_address;

src/router.cpp
@@ -33,14 +33,14 @@ zmq::router_t::router_t (class ctx_t *parent_, uint32_t tid_, int sid_) :
     more_out (false),
     next_rid (generate_random ()),
     mandatory (false),
-    //  raw_sock functionality in ROUTER is deprecated
-    raw_sock (false),       
+    //  raw_socket functionality in ROUTER is deprecated
+    raw_socket (false),       
     probe_router (false),
     handover (false)
 {
     options.type = ZMQ_ROUTER;
     options.recv_identity = true;
-    options.raw_sock = false;
+    options.raw_socket = false;
 
     prefetched_id.init ();
     prefetched_msg.init ();
@@ -96,10 +96,10 @@ int zmq::router_t::xsetsockopt (int option_, const void *optval_,
             break;
         case ZMQ_ROUTER_RAW:
             if (is_int && value >= 0) {
-                raw_sock = (value != 0);
-                if (raw_sock) {
+                raw_socket = (value != 0);
+                if (raw_socket) {
                     options.recv_identity = false;
-                    options.raw_sock = true;
+                    options.raw_socket = true;
                 }
                 return 0;
             }
@@ -223,7 +223,7 @@ int zmq::router_t::xsend (msg_t *msg_)
     }
 
     //  Ignore the MORE flag for raw-sock or assert?
-    if (options.raw_sock)
+    if (options.raw_socket)
         msg_->reset_flags (msg_t::more);
 
     //  Check whether this is the last part of the message.
@@ -235,7 +235,7 @@ int zmq::router_t::xsend (msg_t *msg_)
         // Close the remote connection if user has asked to do so
         // by sending zero length message.
         // Pending messages in the pipe will be dropped (on receiving term- ack)
-        if (raw_sock && msg_->size() == 0) {
+        if (raw_socket && msg_->size() == 0) {
             current_out->terminate (false);
             int rc = msg_->close ();
             errno_assert (rc == 0);
@@ -397,14 +397,14 @@ bool zmq::router_t::identify_peer (pipe_t *pipe_)
             zmq_assert(false); //  Not allowed to duplicate an existing rid
     }
     else 
-    if (options.raw_sock) { //  Always assign identity for raw-socket
+    if (options.raw_socket) { //  Always assign identity for raw-socket
         unsigned char buf [5];
         buf [0] = 0;
         put_uint32 (buf + 1, next_rid++);
         identity = blob_t (buf, sizeof buf);
     }
     else
-    if (!options.raw_sock) { 
+    if (!options.raw_socket) { 
         //  Pick up handshake cases and also case where next identity is set
         msg.init ();
         ok = pipe_->read (&msg);

src/router.hpp
@@ -111,7 +111,7 @@ namespace zmq
         // If true, report EAGAIN to the caller instead of silently dropping 
         // the message targeting an unknown peer.
         bool mandatory;
-        bool raw_sock;
+        bool raw_socket;
 
         // if true, send an empty message to every connected router peer
         bool probe_router;

src/session_base.cpp
@@ -213,14 +213,15 @@ void zmq::session_base_t::pipe_terminated (pipe_t *pipe_)
             cancel_timer (linger_timer_id);
             has_linger_timer = false;
         }
-    } else
+    }
+    else
     if (pipe_ == zap_pipe)
         zap_pipe = NULL;
     else
         // Remove the pipe from the detached pipes set
         terminating_pipes.erase (pipe_);
 
-    if (!is_terminating () && options.raw_sock) {
+    if (!is_terminating () && options.raw_socket) {
         if (engine) {
             engine->terminate ();
             engine = NULL;

src/stream.cpp
@@ -33,7 +33,7 @@ zmq::stream_t::stream_t (class ctx_t *parent_, uint32_t tid_, int sid_) :
     next_rid (generate_random ())
 {
     options.type = ZMQ_STREAM;
-    options.raw_sock = true;
+    options.raw_socket = true;
 
     prefetched_id.init ();
     prefetched_msg.init ();
@@ -167,6 +167,8 @@ int zmq::stream_t::xsend (msg_t *msg_)
 int zmq::stream_t::xsetsockopt (int option_, const void *optval_,
     size_t optvallen_)
 {
+    bool is_int = (optvallen_ == sizeof (int));
+    int value = is_int? *((int *) optval_): 0;
     switch (option_) {
         case ZMQ_CONNECT_RID:
             if (optval_ && optvallen_) {
@@ -174,6 +176,14 @@ int zmq::stream_t::xsetsockopt (int option_, const void *optval_,
                 return 0;
             }
             break;
+            
+        case ZMQ_STREAM_NOTIFY:
+            if (is_int && (value == 0 || value == 1)) {
+                options.raw_notify = value;
+                return 0;
+            }
+            break;
+
         default:
             break;
     }

src/stream_engine.cpp
@@ -180,7 +180,7 @@ void zmq::stream_engine_t::plug (io_thread_t *io_thread_,
     handle = add_fd (s);
     io_error = false;
 
-    if (options.raw_sock) {
+    if (options.raw_socket) {
         // no handshaking for raw sock, instantiate raw encoder and decoders
         encoder = new (std::nothrow) raw_encoder_t (out_batch_size);
         alloc_assert (encoder);
@@ -194,13 +194,15 @@ void zmq::stream_engine_t::plug (io_thread_t *io_thread_,
         next_msg = &stream_engine_t::pull_msg_from_session;
         process_msg = &stream_engine_t::push_msg_to_session;
 
-        //  For raw sockets, send an initial 0-length message to the
-        // application so that it knows a peer has connected.
-        msg_t connector;
-        connector.init();
-        push_msg_to_session (&connector);
-        connector.close();
-        session->flush ();
+        if (options.raw_notify) {
+            //  For raw sockets, send an initial 0-length message to the
+            // application so that it knows a peer has connected.
+            msg_t connector;
+            connector.init();
+            push_msg_to_session (&connector);
+            connector.close();
+            session->flush ();
+        }
     }
     else {
         // start optional timer, to prevent handshake hanging on no input
@@ -914,7 +916,7 @@ int zmq::stream_engine_t::write_subscription_msg (msg_t *msg_)
 
 void zmq::stream_engine_t::error (error_reason_t reason)
 {
-    if (options.raw_sock) {
+    if (options.raw_socket) {
         //  For raw sockets, send a final 0-length message to the application
         //  so that it knows the peer has been disconnected.
         msg_t terminator;
@@ -934,7 +936,7 @@ void zmq::stream_engine_t::set_handshake_timer ()
 {
     zmq_assert (!has_handshake_timer);
 
-    if (!options.raw_sock && options.handshake_ivl > 0) {
+    if (!options.raw_socket && options.handshake_ivl > 0) {
         add_timer (options.handshake_ivl, handshake_timer_id);
         has_handshake_timer = true;
     }

tests/test_connect_rid.cpp
@@ -63,19 +63,13 @@ void test_stream_2_stream(){
 
     //  Accept data on the bound stream.
     ret = zmq_recv (rbind, buff, 256, 0);
-    assert (ret && 0 == buff[0]);
-    assert (0 == buff[0]);
-    ret = zmq_recv (rbind, buff, 256, 0);
-    assert (0 == ret);
-
-    // Handle close of the socket.
-    ret = zmq_recv (rbind, buff, 256, 0);
     assert (ret);
     assert (0 == buff[0]);
     ret = zmq_recv (rbind, buff+128, 128, 0);
-    assert (5 == ret); 
+    assert (5 == ret);
     assert ('h' == buff[128]);
 
+    // Handle close of the socket.
     ret = zmq_unbind (rbind, bindip);
     assert(0 == ret);
     ret = zmq_close (rbind);

tests/test_stream.cpp
@@ -54,6 +54,9 @@ test_stream_to_dealer (void)
     int zero = 0;
     rc = zmq_setsockopt (stream, ZMQ_LINGER, &zero, sizeof (zero));
     assert (rc == 0);
+    int enabled = 1;
+    rc = zmq_setsockopt (stream, ZMQ_STREAM_NOTIFY, &enabled, sizeof (enabled));
+    assert (rc == 0);
     rc = zmq_bind (stream, "tcp://127.0.0.1:5556");
     assert (rc == 0);
 
@@ -182,11 +185,16 @@ test_stream_to_stream (void)
     
     void *server = zmq_socket (ctx, ZMQ_STREAM);
     assert (server);
+    int enabled = 1;
+    rc = zmq_setsockopt (server, ZMQ_STREAM_NOTIFY, &enabled, sizeof (enabled));
+    assert (rc == 0);
     rc = zmq_bind (server, "tcp://127.0.0.1:9070");
     assert (rc == 0);
 
     void *client = zmq_socket (ctx, ZMQ_STREAM);
     assert (client);
+    rc = zmq_setsockopt (client, ZMQ_STREAM_NOTIFY, &enabled, sizeof (enabled));
+    assert (rc == 0);
     rc = zmq_connect (client, "tcp://localhost:9070");
     assert (rc == 0);
     uint8_t id [256];

tests/test_stream_disconnect.cpp
@@ -55,15 +55,20 @@ int main(int, char**)
 {
     setup_test_environment();
 
-    void* context = zmq_ctx_new ();
-    void* sockets [2];
+    void *context = zmq_ctx_new ();
+    void *sockets [2];
     int rc = 0;
 
     sockets [SERVER] = zmq_socket (context, ZMQ_STREAM);
+    int enabled = 1;
+    rc = zmq_setsockopt (sockets [SERVER], ZMQ_STREAM_NOTIFY, &enabled, sizeof (enabled));
+    assert (rc == 0);
     rc = zmq_bind (sockets [SERVER], "tcp://0.0.0.0:6666");
     assert (rc == 0);
 
     sockets [CLIENT] = zmq_socket (context, ZMQ_STREAM);
+    rc = zmq_setsockopt (sockets [CLIENT], ZMQ_STREAM_NOTIFY, &enabled, sizeof (enabled));
+    assert (rc == 0);
     rc = zmq_connect (sockets [CLIENT], "tcp://localhost:6666");
     assert (rc == 0);
 

tests/testutil.hpp
@@ -22,7 +22,7 @@
 
 #include "../include/zmq.h"
 #include "../src/stdint.hpp"
-#include "platform.hpp"
+#include "../src/platform.hpp"
 
 //  This defines the settle time used in tests; raise this if we
 //  get test failures on slower systems due to binds/connects not