148427d291efb8a7c1bb3c7642c46497b6fc5674
Mark Lakata <mlakata@pacb.com>
2015-03-09 17:19:52 -0700

    Update stream_engine.hpp
    The Intel compiler complains about the order of "static" and "const". The fix is a simple switch to one line in one header file.
    
    In file included from ipc_listener.cpp(29):
    stream_engine.hpp(97): error #82: storage class is not first
              const static size_t greeting_size = 12;

--- src/stream_engine.hpp
+++ src/stream_engine.hpp
@@ -94,7 +94,7 @@ namespace zmq
 
         //  Size of the greeting message:
         //  Preamble (10 bytes) + version (1 byte) + socket type (1 byte).
-        const static size_t greeting_size = 12;
+        static const size_t greeting_size = 12;
 
         //  True iff we are registered with an I/O poller.
         bool io_enabled;
