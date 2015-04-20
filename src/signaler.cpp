b589fb98b8d3bd4f10e6a9b7bcf6b8a96e45a8a5
Pieter Hintjens <ph@imatix.com>
2015-04-20 12:51:10 +0200

    Problem: shutdown asserts if WSASTARUP wasn't done previously
    This is a silly assertion that causes problems if libzmq.dll is
    called in some esoteric ways.
    
    Solution: if the shutdown code detects WSANOTINITIALISED, then
    exit silently.
    
    Fixes #1377
    Fixes #1144
    
    Conflicts:
    
    	src/signaler.cpp

--- src/signaler.cpp
+++ src/signaler.cpp
@@ -146,12 +146,15 @@ zmq::signaler_t::~signaler_t ()
 #elif defined ZMQ_HAVE_WINDOWS
     struct linger so_linger = { 1, 0 };
     int rc = setsockopt (w, SOL_SOCKET, SO_LINGER,
-        (char *)&so_linger, sizeof (so_linger));
-    wsa_assert (rc != SOCKET_ERROR);
-    rc = closesocket (w);
-    wsa_assert (rc != SOCKET_ERROR);
-    rc = closesocket (r);
-    wsa_assert (rc != SOCKET_ERROR);
+        (const char *) &so_linger, sizeof so_linger);
+    //  Only check shutdown if WSASTARTUP was previously done
+    if (rc == 0 || WSAGetLastError () != WSANOTINITIALISED) {
+        wsa_assert (rc != SOCKET_ERROR);
+        rc = closesocket (w);
+        wsa_assert (rc != SOCKET_ERROR);
+        rc = closesocket (r);
+        wsa_assert (rc != SOCKET_ERROR);
+    }
 #else
     int rc = close_wait_ms (w);
     errno_assert (rc == 0);
