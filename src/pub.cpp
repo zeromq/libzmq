a1d1222dcc74d66541e3fcf7b352f98532a83a4f
Martin Hurton <hurtonm@gmail.com>
2015-04-28 07:44:22 +0200

    pub: Don't delay pipe termination

--- src/pub.cpp
+++ src/pub.cpp
@@ -18,6 +18,8 @@
 */
 
 #include "pub.hpp"
+#include "pipe.hpp"
+#include "err.hpp"
 #include "msg.hpp"
 
 zmq::pub_t::pub_t (class ctx_t *parent_, uint32_t tid_, int sid_) :
@@ -28,6 +30,17 @@ zmq::pub_t::pub_t (class ctx_t *parent_, uint32_t tid_, int sid_) :
 
 zmq::pub_t::~pub_t ()
 {
+}
+
+void zmq::pub_t::xattach_pipe (pipe_t *pipe_, bool subscribe_to_all_)
+{
+    zmq_assert (pipe_);
+
+    //  Don't delay pipe termination as there is no one
+    //  to receive the delimiter.
+    pipe_->set_nodelay ();
+
+    xpub_t::xattach_pipe (pipe_, subscribe_to_all_);
 }
 
 int zmq::pub_t::xrecv (class msg_t *)
