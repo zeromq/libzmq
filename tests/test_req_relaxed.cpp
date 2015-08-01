a228da62b1ae65066ad8d437500533b3f9100630
Luca Boccassi <luca.boccassi@gmail.com>
2015-08-01 16:11:50 +0100

    Problem: CI may fail due to timeout. Fixes #135
    Solution: cherry-pick Pieter Hintjens commit a7a512ab from libzmq
    repository to increase test timeouts to 250 msec.

--- tests/test_req_relaxed.cpp
+++ tests/test_req_relaxed.cpp
@@ -44,7 +44,7 @@ int main (void)
         rep [peer] = zmq_socket (ctx, ZMQ_REP);
         assert (rep [peer]);
 
-        int timeout = 100;
+        int timeout = 250;
         rc = zmq_setsockopt (rep [peer], ZMQ_RCVTIMEO, &timeout, sizeof (int));
         assert (rc == 0);
 
