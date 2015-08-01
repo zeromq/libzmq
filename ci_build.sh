20ba66101970142b171b021c8e14f7237590672d
Luca Boccassi <luca.boccassi@gmail.com>
2015-08-01 18:53:11 +0100

    Do not use ldconfig in CI if running on OSX
    ldconfig is not available on OSX, so Travis CI build fails

--- ci_build.sh
+++ ci_build.sh
@@ -5,7 +5,8 @@ if [ $BUILD_TYPE == "default" ]; then
 
     #   libsodium
     git clone git://github.com/jedisct1/libsodium.git
-    ( cd libsodium; ./autogen.sh; ./configure; make check; sudo make install; sudo ldconfig )
+    ( cd libsodium; ./autogen.sh; ./configure; make check; sudo make install;
+        if [ $TRAVIS_OS_NAME != "osx" ] ; then sudo ldconfig ; fi )
 
     #   Build and check this project
     ./autogen.sh && ./configure --with-libsodium=yes && make && make check
