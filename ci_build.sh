1afc30a6c4b0b37a7fb7f7547c12ecca4598aad6
Luca Boccassi <luca.boccassi@gmail.com>
2015-08-16 18:46:03 +0100

    CI build/install in local directory

--- ci_build.sh
+++ ci_build.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
 if [ $BUILD_TYPE == "default" ]; then
+    mkdir tmp
+    BUILD_PREFIX=$PWD/tmp
+
+    CONFIG_OPTS=()
+    CONFIG_OPTS+=("CFLAGS=-I${BUILD_PREFIX}/include")
+    CONFIG_OPTS+=("CPPFLAGS=-I${BUILD_PREFIX}/include")
+    CONFIG_OPTS+=("CXXFLAGS=-I${BUILD_PREFIX}/include")
+    CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
+    CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
+    CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
+
     #   Build required projects first
 
     #   libsodium
     git clone git://github.com/jedisct1/libsodium.git
-    ( cd libsodium; ./autogen.sh; ./configure; make check; sudo make install;
-        if [ $TRAVIS_OS_NAME != "osx" ] ; then sudo ldconfig ; fi )
+    ( cd libsodium; ./autogen.sh; ./configure --prefix=$BUILD_PREFIX; make check; make install)
 
     #   Build and check this project
-    (./autogen.sh && ./configure --with-libsodium=yes && make && make check && sudo make install) || exit 1
+    (./autogen.sh && ./configure "${CONFIG_OPTS[@]}" --with-libsodium=yes && make && make check && make install) || exit 1
 else
     cd ./builds/${BUILD_TYPE} && ./ci_build.sh
 fi
