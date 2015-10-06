3db502ba47e0332d52e15a7043983a94b2914a1d
Jakub Stefanski <iakub.stefanski+git@gmail.com>
2015-10-06 14:17:54 +0200

    Fix FindAsciiDoc module for CMake 3
    Fixed '(' and ')' braces usage volation according to CMake
    policy CMP0053 in FindAsciiDoc module.

--- builds/cmake/Modules/FindAsciiDoc.cmake
+++ builds/cmake/Modules/FindAsciiDoc.cmake
@@ -6,17 +6,20 @@
 # A2X_EXECUTABLE - the full path to a2x
 # A2X_FOUND - If false, don't attempt to use a2x.
 
+# CMP0053: Unable to refer to $ENV{PROGRAMFILES(X86)}
+set(PROGRAMFILESX86 "PROGRAMFILES(X86)")
+
 find_program(ASCIIDOC_EXECUTABLE asciidoc asciidoc.py
              PATHS "$ENV{ASCIIDOC_ROOT}"
                    "$ENV{PROGRAMW6432}/asciidoc"
                    "$ENV{PROGRAMFILES}/asciidoc"
-                   "$ENV{PROGRAMFILES(X86)}/asciidoc")
+                   "$ENV{${PROGRAMFILESX86}}/asciidoc")
 
 find_program(A2X_EXECUTABLE a2x
              PATHS "$ENV{ASCIIDOC_ROOT}"
                    "$ENV{PROGRAMW6432}/asciidoc"
                    "$ENV{PROGRAMFILES}/asciidoc"
-                   "$ENV{PROGRAMFILES(X86)}/asciidoc")
+                   "$ENV{${PROGRAMFILESX86}}/asciidoc")
 
 
 include(FindPackageHandleStandardArgs)
