b2c87b9a70fc11d7406037e0ff9a236e53396438
Anonymous Maarten <anonymous.maarten@gmail.com>
2015-10-23 16:13:53 +0200

    TweetNaCL: add windows randombytes implementation

--- tweetnacl/contrib/randombytes/randombytes.h
+++ tweetnacl/contrib/randombytes/randombytes.h
@@ -1,5 +1,21 @@
+/*
+randombytes/randombytes.h version 20080713
+D. J. Bernstein
+Public domain.
+*/
 
 #ifndef randombytes_H
 #define randombytes_H
-#include "devurandom.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void randombytes(unsigned char *,unsigned long long);
+extern int randombytes_close(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
