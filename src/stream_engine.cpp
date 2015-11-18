210572201d2b7c7ab249ce2741ad69b287d2d28b
somdoron <somdoron@gmail.com>
2015-11-18 15:02:19 +0200

    Fix a bug when stream_engine try to set alreadt set metadata

--- src/stream_engine.cpp
+++ src/stream_engine.cpp
@@ -869,7 +869,7 @@ int zmq::stream_engine_t::push_msg_to_session (msg_t *msg_)
 }
 
 int zmq::stream_engine_t::push_raw_msg_to_session (msg_t *msg_) {
-    if (metadata)
+    if (metadata && metadata != msg_->metadata())
         msg_->set_metadata(metadata);
     return push_msg_to_session(msg_);
 }
