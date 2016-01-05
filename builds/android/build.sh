c49eef46ec18a86a9471bb172845ef2c21219b1a
Pieter Hintjens <ph@imatix.com>
2016-01-05 17:31:17 +0100

    Problem: Android build is too slow
    Solution: use parallel make (-j 4) and don't build man pages.

--- builds/android/build.sh
+++ builds/android/build.sh
@@ -33,7 +33,7 @@ fi
     (cd "${cache}" && git clone --depth 1 git://github.com/jedisct1/libsodium.git) || exit 1
     (cd "${cache}/libsodium" && ./autogen.sh \
         && ./configure "${ANDROID_BUILD_OPTS[@]}" --disable-soname-versions \
-        && make \
+        && make -j 4 \
         && make install) || exit 1
 }
 
@@ -47,8 +47,8 @@ LIBTOOL_EXTRA_LDFLAGS='-avoid-version'
     (cp -r ../.. "${cache}/libzmq" && cd "${cache}/libzmq" && make clean)
     
     (cd "${cache}/libzmq" && ./autogen.sh \
-        && ./configure "${ANDROID_BUILD_OPTS[@]}" --with-libsodium=yes \
-        && make \
+        && ./configure "${ANDROID_BUILD_OPTS[@]}" --without-documentation --with-libsodium=yes \
+        && make -j 4 \
         && make install) || exit 1
 }
 
