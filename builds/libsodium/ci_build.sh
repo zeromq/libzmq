3420e6e0ddbc1acee1a85051f94519adca9cdff0
Luca Boccassi <luca.boccassi@gmail.com>
2016-02-11 20:12:10 +0000

    Problem: CI still thinks libsodium is default
    Solution: build by default with tweetnacl, and add sub-build project
    to test libsodium

--- builds/libsodium/ci_build.sh
+++ builds/libsodium/ci_build.sh
@@ -13,11 +13,17 @@ CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
 CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
 CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
 
+#   Build required projects first
+
+#   libsodium
+git clone --depth 1 -b stable git://github.com/jedisct1/libsodium.git
+( cd libsodium; ./autogen.sh; ./configure --prefix=$BUILD_PREFIX; make check; make install)
+
 #   Build and check this project
 (
     cd ../..;
     ./autogen.sh &&
-    ./configure "${CONFIG_OPTS[@]}" --with-tweetnacl=yes &&
+    ./configure "${CONFIG_OPTS[@]}" --with-libsodium=yes &&
     make &&
     ( if make check; then true; else cat test-suite.log; exit 1; fi ) &&
     make install
