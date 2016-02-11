0bdc5d0457688b6a07bbd76dbafb04fb2d779a22
Luca Boccassi <luca.boccassi@gmail.com>
2016-02-11 20:27:06 +0000

    Problem: CMake CI build still clones libsodium
    Solution: avoid cloning libsodium, default build is tweetnacl

--- builds/cmake/ci_build.sh
+++ builds/cmake/ci_build.sh
@@ -18,8 +18,5 @@ CMAKE_OPTS+=("-DCMAKE_PREFIX_PATH:PATH=${BUILD_PREFIX}")
 CMAKE_OPTS+=("-DCMAKE_LIBRARY_PATH:PATH=${BUILD_PREFIX}/lib")
 CMAKE_OPTS+=("-DCMAKE_INCLUDE_PATH:PATH=${BUILD_PREFIX}/include")
 
-git clone --depth 1 -b stable git://github.com/jedisct1/libsodium.git &&
-( cd libsodium; ./autogen.sh && ./configure --prefix=$BUILD_PREFIX && make && make install ) || exit 1
-
 # Build, check, and install from local source
 ( cd ../..; mkdir build_cmake && cd build_cmake && PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig cmake "${CMAKE_OPTS[@]}" .. && make all VERBOSE=1 && make install ) || exit 1
