80024eca8c70ac2ff075fa04581961326e01e251
Osiris <opedroso@gmail.com>
2016-02-17 09:42:53 -0600

    Problem:  Windows build script requires edit for DevStudio version
    Solution: Use CMD.EXE environment variable to extract DevStudio version
    number and build using it. This even supports machines with multiple
    DevStudio versions installed, as long as the build for each version is
    done on a separate window with the correct environment.
    If multiple version builds are desired from a single CMD.EXE, edit the
    buildall.bat file to uncomment the build statements for each specific
    version desired.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,7 +1,17 @@
 @ECHO OFF
+@setlocal
+::
+:: uses the environment from the DevStudio CMD window to figure out which version to build
+::
 
-CALL buildbase.bat ..\vs2015\libzmq.sln 14
-ECHO.
+set VSVER=%VSINSTALLDIR:~-5,2%
+set DIRVER=%VSVER%
+if %VSVER% gtr 10 set /a DIRVER = DIRVER + 1
+
+CALL buildbase.bat ..\vs20%DIRVER%\libzmq.sln %VSVER%
+
+:- CALL buildbase.bat ..\vs2015\libzmq.sln 14
+:- ECHO.
 :- CALL buildbase.bat ..\vs2013\libzmq.sln 12
 :- ECHO.
 :- CALL buildbase.bat ..\vs2012\libzmq.sln 11
@@ -9,4 +19,5 @@ ECHO.
 :- CALL buildbase.bat ..\vs2010\libzmq.sln 10
 :- ECHO.
 
+@endlocal
 PAUSE
