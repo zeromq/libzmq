09d003aac0826f88e1ed4b5defca37741c4b90a7
Osiris <opedroso@gmail.com>
2016-03-05 04:37:01 -0600

    Problem: Added "make clean" to Windows build scripts
    Solution: Added ability to do equivalent of a "make clean" by executing:
    
    O:\git\libzmq\builds\msvc\build>buildall.bat clean
    Start Time: Fri 03/04/2016 14:33:56.50
    Cleaning without libsodium
    Cleaning ..\vs2013\libzmq.sln... ()
    Platform=x86
    Configuration=DynDebug
    Configuration=DynRelease
    Configuration=LtcgDebug
    Configuration=LtcgRelease
    Configuration=StaticDebug
    Configuration=StaticRelease
    Platform=x64
    Configuration=DynDebug
    Configuration=DynRelease
    Configuration=LtcgDebug
    Configuration=LtcgRelease
    Configuration=StaticDebug
    Configuration=StaticRelease
    Cleaning complete: ..\vs2013\libzmq.sln
    
    Stop  Time: Fri 03/04/2016 14:34:01.84
    Start Time: Fri 03/04/2016 14:33:56.50
    
    A clean takes ~5 secs to complete.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,5 +1,20 @@
 @ECHO OFF
+:: Usage: buildall.bat [Clean]
 @setlocal
+
+:: validate environment
+if "%VSINSTALLDIR%" == "" @echo Error: Attempt to build without proper DevStudio environment.&@goto :done
+
+:: record starting time
+set STARTTIME=%DATE% %TIME%
+@echo Start Time: %STARTTIME%
+
+
+:: validate optional argument (and make sure it is spelled "Clean")
+set MAKECLEAN=%%1
+if NOT "%%1" == "" if /I "%%1" == "clean" set MAKECLEAN=Clean
+
+
 ::
 :: uses the environment from the DevStudio CMD window to figure out which version to build
 ::
@@ -8,16 +23,22 @@ set VSVER=%VSINSTALLDIR:~-5,2%
 set DIRVER=%VSVER%
 if %VSVER% gtr 10 set /a DIRVER = DIRVER + 1
 
-CALL buildbase.bat ..\vs20%DIRVER%\libzmq.sln %VSVER%
+CALL buildbase.bat ..\vs20%DIRVER%\libzmq.sln %VSVER% %MAKECLEAN%
 
-:- CALL buildbase.bat ..\vs2015\libzmq.sln 14
+:- CALL buildbase.bat ..\vs2015\libzmq.sln 14 %MAKECLEAN%
 :- ECHO.
-:- CALL buildbase.bat ..\vs2013\libzmq.sln 12
+:- CALL buildbase.bat ..\vs2013\libzmq.sln 12 %MAKECLEAN%
 :- ECHO.
-:- CALL buildbase.bat ..\vs2012\libzmq.sln 11
+:- CALL buildbase.bat ..\vs2012\libzmq.sln 11 %MAKECLEAN%
 :- ECHO.
-:- CALL buildbase.bat ..\vs2010\libzmq.sln 10
+:- CALL buildbase.bat ..\vs2010\libzmq.sln 10 %MAKECLEAN%
 :- ECHO.
 
+set STOPTIME=%DATE% %TIME%
+@echo Stop  Time: %STOPTIME%
+@echo Start Time: %STARTTIME%
+
+:done
 @endlocal
+
 PAUSE
