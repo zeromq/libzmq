6c3129286d6b4f816a8d77044c9507b464ebab5d
hitstergtd <hitstergtd@users.noreply.github.com>
2016-04-14 21:02:53 +0100

    Fix typo

--- src/ypipe.hpp
+++ src/ypipe.hpp
@@ -61,7 +61,7 @@ namespace zmq
             c.set (&queue.back ());
         }
 
-        //  The destructor doesn't have to be virtual. It is mad virtual
+        //  The destructor doesn't have to be virtual. It is made virtual
         //  just to keep ICC and code checking tools from complaining.
         inline virtual ~ypipe_t ()
         {
