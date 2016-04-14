a17389eeb09edb4a4098ddb70a663f0b9217092b
hitstergtd <hitstergtd@users.noreply.github.com>
2016-04-14 21:10:12 +0100

    Fix typos similar to #1894

--- src/ypipe_conflate.hpp
+++ src/ypipe_conflate.hpp
@@ -54,7 +54,7 @@ namespace zmq
         {
         }
 
-        //  The destructor doesn't have to be virtual. It is mad virtual
+        //  The destructor doesn't have to be virtual. It is made virtual
         //  just to keep ICC and code checking tools from complaining.
         inline virtual ~ypipe_conflate_t ()
         {
