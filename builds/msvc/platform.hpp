f25a13bc8aac5e81161123c875e11b88fd41c5d6
evoskuil <eric@voskuil.org>
2016-05-12 15:36:18 -0700

    Problem: configure.bat is incomplete and conflicts with MSVC UI.

--- builds/msvc/platform.hpp
+++ builds/msvc/platform.hpp
@@ -1,1 +1,14 @@
-#error "Run configure.bat to create platform.h"
+#ifndef __PLATFORM_H_INCLUDED__
+#define __PLATFORM_H_INCLUDED__
+
+#define ZMQ_HAVE_WINDOWS
+
+// MSVC build configuration is controlled via options exposed in the Visual
+// Studio user interface. The option to use libsodium is not exposed in the
+// user interface unless a sibling `libsodium` directory to the that of this
+// repository exists and contains the following files:
+//
+// \builds\msvc\vs2015\libsodium.import.props
+// \builds\msvc\vs2015\libsodium.import.xml
+
+#endif
