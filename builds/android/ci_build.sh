8be568916e4f2cc1d934398ecb470baab7841a51
Luca Boccassi <luca.boccassi@gmail.com>
2016-06-07 23:18:21 +0100

    Problem: Android CI script hardcodes ABI version
    Solution: use a variable for easier maintenance, in sync with zproject

--- builds/android/ci_build.sh
+++ builds/android/ci_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 NDK_VER=android-ndk-r10e
+NDK_ABI_VER=4.9
 
 if [ $TRAVIS_OS_NAME == "linux" ]
 then
@@ -22,8 +23,8 @@ export FILENAME=$NDK_VER-$NDK_PLATFORM.bin
 unset FILENAME
 
 export ANDROID_NDK_ROOT="/tmp/$NDK_VER"
-export TOOLCHAIN_PATH="$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/$NDK_PLATFORM/bin"
-export TOOLCHAIN_NAME="arm-linux-androideabi-4.8"
+export TOOLCHAIN_PATH="$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-$NDK_ABI_VER/prebuilt/$NDK_PLATFORM/bin"
+export TOOLCHAIN_NAME="arm-linux-androideabi-$NDK_ABI_VER"
 export TOOLCHAIN_HOST="arm-linux-androideabi"
 export TOOLCHAIN_ARCH="arm"
 
