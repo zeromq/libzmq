8d8d32f4d058d5f4723e8ed714ff36937f9fa6fa
Michael Lutz <michi@icosahedron.de>
2016-06-11 18:50:40 +0200

    Problem: Theoretical overflow when polling more than INT_MAX handles.
    Solution: Always use fd_t when accessing the pollset.

--- src/poll.cpp
+++ src/poll.cpp
@@ -94,25 +94,25 @@ void zmq::poll_t::rm_fd (handle_t handle_)
 
 void zmq::poll_t::set_pollin (handle_t handle_)
 {
-    int index = fd_table [handle_].index;
+    fd_t index = fd_table [handle_].index;
     pollset [index].events |= POLLIN;
 }
 
 void zmq::poll_t::reset_pollin (handle_t handle_)
 {
-    int index = fd_table [handle_].index;
+    fd_t index = fd_table [handle_].index;
     pollset [index].events &= ~((short) POLLIN);
 }
 
 void zmq::poll_t::set_pollout (handle_t handle_)
 {
-    int index = fd_table [handle_].index;
+    fd_t index = fd_table [handle_].index;
     pollset [index].events |= POLLOUT;
 }
 
 void zmq::poll_t::reset_pollout (handle_t handle_)
 {
-    int index = fd_table [handle_].index;
+    fd_t index = fd_table [handle_].index;
     pollset [index].events &= ~((short) POLLOUT);
 }
 
