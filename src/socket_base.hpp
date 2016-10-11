d1c7280d05834d9e165757973d5d79eb18dca517
Jens Auer <jens.auer@cgi.com>
2016-10-11 13:28:45 +0000

    Add mutex for monitor socket

--- src/socket_base.hpp
+++ src/socket_base.hpp
@@ -176,16 +176,20 @@ namespace zmq
         //  Delay actual destruction of the socket.
         void process_destroy ();
 
+
+        // Next assigned name on a zmq_connect() call used by ROUTER and STREAM socket types
+        std::string connect_rid;
+
+    private:
+        // test if event should be sent and then dispatch it        
+        void event(const std::string &addr_, int fd_, int type_);
+
         // Socket event data dispatch
         void monitor_event (int event_, intptr_t value_, const std::string& addr_);
 
         // Monitor socket cleanup
         void stop_monitor (bool send_monitor_stopped_event_ = true);
 
-        // Next assigned name on a zmq_connect() call used by ROUTER and STREAM socket types
-        std::string connect_rid;
-
-    private:
         //  Creates new endpoint ID and adds the endpoint to the map.
         void add_endpoint (const char *addr_, own_t *endpoint_, pipe_t *pipe);
 
@@ -281,6 +285,9 @@ namespace zmq
 
         // Mutex for synchronize access to the socket in thread safe mode
         mutex_t sync;
+
+        // Mutex to synchronize access to the monitor Pair socket
+        mutex_t monitor_sync;
 
         socket_base_t (const socket_base_t&);
         const socket_base_t &operator = (const socket_base_t&);
