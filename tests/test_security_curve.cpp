3594f7a8fa9fe5c764f54cd1d758684569880140
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-08-04 16:50:09 +0200

    Problem: missing test for status code 300, inadequate assertion for status code 500
    Solution: add test, change assertion (currently test fails)

--- tests/test_security_curve.cpp
+++ tests/test_security_curve.cpp
@@ -140,6 +140,7 @@ enum zap_protocol_t
 {
   zap_ok,
   // ZAP-compliant non-standard cases
+  zap_status_temporary_failure,
   zap_status_internal_error,
   // ZAP protocol errors
   zap_wrong_version,
@@ -180,11 +181,21 @@ static void zap_handler_generic (void *handler, zap_protocol_t zap_protocol)
                                : sequence);
 
         if (streq (client_key_text, valid_client_public)) {
-            s_sendmore (handler, zap_protocol == zap_status_internal_error
-                                   ? "500"
-                                   : (zap_protocol == zap_status_invalid
-                                        ? "invalid_status"
-                                        : "200"));
+            char *status_code;
+            switch (zap_protocol) {
+                case zap_status_internal_error:
+                    status_code = "500";
+                    break;
+                case zap_status_temporary_failure:
+                    status_code = "300";
+                    break;
+                case zap_status_invalid:
+                    status_code = "invalid_status";
+                    break;
+                default:
+                    status_code = "200";
+            }
+            s_sendmore (handler, status_code);
             s_sendmore (handler, "OK");
             s_sendmore (handler, "anonymous");
             if (zap_protocol == zap_too_many_parts) {
@@ -227,6 +238,11 @@ static void zap_handler_wrong_request_id (void *handler)
 static void zap_handler_wrong_status_invalid (void *handler)
 {
     zap_handler_generic (handler, zap_status_invalid);
+}
+
+static void zap_handler_wrong_status_temporary_failure (void *handler)
+{
+    zap_handler_generic (handler, zap_status_temporary_failure);
 }
 
 static void zap_handler_wrong_status_internal_error (void *handler)
@@ -286,6 +302,7 @@ int expect_monitor_event_multiple (void *server_mon,
 {
     int count_of_expected_events = 0;
     int client_closed_connection = 0;
+    //  infinite timeout at the start
     int timeout = -1;
 
     int event;
@@ -725,17 +742,28 @@ int main (void)
 
     //  ZAP non-standard cases
 
-    //  status 500 internal error
+    //  status 300 temporary failure
     setup_context_and_server_side (&ctx, &handler, &zap_thread, &server,
                                    &server_mon, my_endpoint,
-                                   &zap_handler_wrong_status_internal_error);
-
-    //  TODO is this usable? EAGAIN does not appear to be an appropriate error 
-    //  code, and the status text is completely lost
+                                   &zap_handler_wrong_status_temporary_failure);
     test_curve_security_zap_unsuccessful (ctx, my_endpoint, server, server_mon,
 #ifdef ZMQ_BUILD_DRAFT_API
                                           ZMQ_EVENT_HANDSHAKE_FAILED_NO_DETAIL,
                                           EAGAIN
+#else
+                                          0, 0
+#endif
+    );
+    shutdown_context_and_server_side (ctx, zap_thread, server, server_mon);
+
+    //  status 500 internal error
+    setup_context_and_server_side (&ctx, &handler, &zap_thread, &server,
+                                   &server_mon, my_endpoint,
+                                   &zap_handler_wrong_status_internal_error);
+    test_curve_security_zap_unsuccessful (ctx, my_endpoint, server, server_mon,
+#ifdef ZMQ_BUILD_DRAFT_API
+                                          ZMQ_EVENT_HANDSHAKE_FAILED_NO_DETAIL,
+                                          EFAULT
 #else
                                           0, 0
 #endif
