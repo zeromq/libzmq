16dd37bbffb3a2300024d3ad3806b0e937f6520d
evoskuil <eric@voskuil.org>
2017-08-11 16:07:54 -0700

    Problem: unused variable causing warnings or breaks.

--- perf/remote_thr.cpp
+++ perf/remote_thr.cpp
@@ -34,7 +34,6 @@
 
 // keys are arbitrary but must match local_lat.cpp
 const char server_pubkey[] = "DX4nh=yUn{-9ugra0X3Src4SU-4xTgqxcYY.+<SH";
-const char server_prvkey[] = "{X}#>t#jRGaQ}gMhv=30r(Mw+87YGs+5%kh=i@f8";
 const char client_pubkey[] = "<n^oA}I:66W+*ds3tAmi1+KJzv-}k&fC2aA5Bj0K";
 const char client_prvkey[] = "9R9bV}[6z6DC-%$!jTVTKvWc=LEL{4i4gzUe$@Zx";
 
