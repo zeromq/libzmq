4161793d1907ce7402b42f9c96e6f9745b0e2a3a
sigiesec <simon.giesecke@btc-ag.com>
2018-02-01 14:18:02 +0100

    Problem: clang-format not run on CI
    Solution: add clang-format to cmake, and add another travis-ci build type

--- builds/cmake/ci_build.sh
+++ builds/cmake/ci_build.sh
@@ -31,4 +31,11 @@ elif [ $CURVE == "libsodium" ]; then
 fi
 
 # Build, check, and install from local source
-( cd ../..; mkdir build_cmake && cd build_cmake && PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig cmake "${CMAKE_OPTS[@]}" .. && make -j5 all VERBOSE=1 && make install && make -j5 test ) || exit 1
+if [ "$DO_CLANG_FORMAT_CHECK" -eq "1" ] ; then
+    if ! (cd ../..; mkdir build_cmake && cd build_cmake && PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig cmake "${CMAKE_OPTS[@]}" .. && make clang-format-check) ; then 
+        make clang-format-diff
+        exit 1
+    fi
+else
+    ( cd ../..; mkdir build_cmake && cd build_cmake && PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig cmake "${CMAKE_OPTS[@]}" .. && make -j5 all VERBOSE=1 && make install && make -j5 test ) || exit 1
+fi
