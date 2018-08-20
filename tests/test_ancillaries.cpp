261dd1f92c72bdc98925b12378efc5e3e645bbee
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-08-20 11:53:09 +0200

    Problem: test_ancillarries not yet using unity
    Solution: migrate to unity

--- tests/test_ancillaries.cpp
+++ tests/test_ancillaries.cpp
@@ -35,16 +35,37 @@
  */
 
 #include "testutil.hpp"
+#include "testutil_unity.hpp"
 
-int main (void)
+void setUp ()
+{
+}
+
+void tearDown ()
+{
+}
+
+void test_version ()
 {
     int major, minor, patch;
 
     zmq_version (&major, &minor, &patch);
-    assert (major == ZMQ_VERSION_MAJOR && minor == ZMQ_VERSION_MINOR
-            && patch == ZMQ_VERSION_PATCH);
+    TEST_ASSERT_EQUAL_INT (ZMQ_VERSION_MAJOR, major);
+    TEST_ASSERT_EQUAL_INT (ZMQ_VERSION_MINOR, minor);
+    TEST_ASSERT_EQUAL_INT (ZMQ_VERSION_PATCH, patch);
+}
 
+void test_strerrror ()
+{
     assert (zmq_strerror (EINVAL));
+}
 
-    return 0;
+int main ()
+{
+    setup_test_environment ();
+
+    UNITY_BEGIN ();
+    RUN_TEST (test_version);
+    RUN_TEST (test_strerrror);
+    return UNITY_END ();
 }
