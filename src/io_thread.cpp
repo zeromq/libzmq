f83b13b2d6e364d2da0b12c09879e0042c290dd5
Luca Boccassi <bluca@debian.org>
2019-02-17 19:27:50 +0000

    Problem: build fails on VS2010 as it does not have snprintf
    Solution: ifdef it

--- src/io_thread.cpp
+++ src/io_thread.cpp
@@ -56,9 +56,11 @@ zmq::io_thread_t::~io_thread_t ()
 
 void zmq::io_thread_t::start ()
 {
-    char name[16];
+    char name[16] = "";
+#ifndef ZMQ_HAVE_WINDOWS
     snprintf (name, sizeof (name), "IO/%u",
               get_tid () - zmq::ctx_t::reaper_tid - 1);
+#endif
     //  Start the underlying I/O thread.
     _poller->start (name);
 }
