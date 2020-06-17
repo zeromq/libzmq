be24c51c4377c38ec2140e652a8aebbd03f5816f
Bill Torpey <bill.torpey@itiviti.com>
2020-06-17 15:29:07 -0400

    revert to master

--- tests/test_stream_timeout.cpp
+++ tests/test_stream_timeout.cpp
@@ -48,7 +48,7 @@ static void test_stream_handshake_timeout_accept ()
       zmq_setsockopt (stream, ZMQ_LINGER, &zero, sizeof (zero)));
 
     //  We'll be using this socket to test TCP stream handshake timeout
-    void *dealer = test_context_socket (ZMQ_SUB);
+    void *dealer = test_context_socket (ZMQ_DEALER);
     TEST_ASSERT_SUCCESS_ERRNO (
       zmq_setsockopt (dealer, ZMQ_LINGER, &zero, sizeof (zero)));
     int val, tenth = 100;
@@ -110,7 +110,7 @@ static void test_stream_handshake_timeout_connect ()
     bind_loopback_ipv4 (stream, my_endpoint, sizeof my_endpoint);
 
     //  We'll be using this socket to test TCP stream handshake timeout
-    void *dealer = test_context_socket (ZMQ_SUB);
+    void *dealer = test_context_socket (ZMQ_DEALER);
     TEST_ASSERT_SUCCESS_ERRNO (
       zmq_setsockopt (dealer, ZMQ_LINGER, &zero, sizeof (zero)));
     int val, tenth = 100;
@@ -136,7 +136,7 @@ static void test_stream_handshake_timeout_connect ()
 
     TEST_ASSERT_SUCCESS_ERRNO (zmq_socket_monitor (
       dealer, "inproc://monitor-dealer",
-      ZMQ_EVENT_ALL));
+      ZMQ_EVENT_CONNECTED | ZMQ_EVENT_DISCONNECTED | ZMQ_EVENT_ACCEPTED));
 
     //  Connect to the inproc endpoint so we'll get events
     TEST_ASSERT_SUCCESS_ERRNO (
@@ -145,18 +145,11 @@ static void test_stream_handshake_timeout_connect ()
     // connect dealer socket to non-sending stream socket
     TEST_ASSERT_SUCCESS_ERRNO (zmq_connect (dealer, my_endpoint));
 
-    int value;
-    char *event_address;
-    int event = get_monitor_event_with_timeout (dealer_mon, &value, &event_address,
-                                             2 * 1000);
-    int limit = 0;
-    while ((event != -1) && (++limit < 1000)) {
-        const char* eventName = get_zmqEventName(event);
-        printf("Got event: %s\n", eventName);
-        //print_unexpected_event_stderr (event, rc, 0, -1);
-        event = get_monitor_event_with_timeout (dealer_mon, &value, &event_address,
-                                             2 * 1000);
-    }
+    // we should get ZMQ_EVENT_CONNECTED and then ZMQ_EVENT_DISCONNECTED
+    int event = get_monitor_event (dealer_mon, NULL, NULL);
+    TEST_ASSERT_EQUAL_INT (ZMQ_EVENT_CONNECTED, event);
+    event = get_monitor_event (dealer_mon, NULL, NULL);
+    TEST_ASSERT_EQUAL_INT (ZMQ_EVENT_DISCONNECTED, event);
 
     test_context_socket_close (dealer);
     test_context_socket_close (dealer_mon);
