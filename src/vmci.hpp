8fe5b54b8a95b67c5fc4b1b5ef9859ec29506a61
Luca Boccassi <bluca@debian.org>
2021-02-20 18:08:25 +0000

    Problem: VMCI build broken
    Solution: refactor it

--- src/vmci.hpp
+++ src/vmci.hpp
@@ -59,6 +59,10 @@ void tune_vmci_connect_timeout (ctx_t *context_,
                                 fd_t sockfd_,
                                 struct timeval timeout_);
 #endif
+
+fd_t vmci_open_socket (const char *address_,
+                       const options_t &options_,
+                       vmci_address_t *out_vmci_addr_);
 }
 
 #endif
--- src/vmci_address.hpp
+++ src/vmci_address.hpp
@@ -43,24 +43,27 @@ namespace zmq
 class vmci_address_t
 {
   public:
+    vmci_address_t ();
     vmci_address_t (ctx_t *parent_);
     vmci_address_t (const sockaddr *sa, socklen_t sa_len, ctx_t *parent_);
-    ~vmci_address_t ();
 
     //  This function sets up the address for VMCI transport.
     int resolve (const char *path_);
 
     //  The opposite to resolve()
-    int to_string (std::string &addr_);
+    int to_string (std::string &addr_) const;
 
+#if defined ZMQ_HAVE_WINDOWS
+    unsigned short family () const;
+#else
+    sa_family_t family () const;
+#endif
     const sockaddr *addr () const;
     socklen_t addrlen () const;
 
   private:
     struct sockaddr_vm address;
     ctx_t *parent;
-
-    vmci_address_t ();
 
     ZMQ_NON_COPYABLE_NOR_MOVABLE (vmci_address_t)
 };
