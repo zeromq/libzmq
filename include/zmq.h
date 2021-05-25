b9d5369b29f86a410f667ae25905de7ed236e8f1
Tobias Schlüter <tobias@lp-research.com>
2021-05-25 11:49:37 +0900

    Remove Windows-specific defines from zmq.h.
    These are redundant with the ones in windows.hpp
    and aren't needed for the public interface.

--- include/zmq.h
+++ include/zmq.h
@@ -57,21 +57,6 @@ extern "C" {
 #endif
 #include <stddef.h>
 #include <stdio.h>
-#if defined _WIN32
-//  Set target version to Windows Server 2008, Windows Vista or higher.
-//  Windows XP (0x0501) is supported but without client & server socket types.
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
-#endif
-
-#ifdef __MINGW32__
-//  Require Windows XP or higher with MinGW for getaddrinfo().
-#if (_WIN32_WINNT >= 0x0501)
-#else
-#error You need at least Windows XP target
-#endif
-#endif
-#endif
 
 /*  Handle DSO symbol visibility                                             */
 #if defined _WIN32
