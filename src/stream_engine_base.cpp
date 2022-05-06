0c222d734009ce86ab624cccf3ff90470598fbf2
ABlueLight <39207427+ABlueLight@users.noreply.github.com>
2022-05-06 10:24:23 +0800

    Update stream_engine_base.cpp

--- src/stream_engine_base.cpp
+++ src/stream_engine_base.cpp
@@ -756,7 +756,7 @@ void zmq::stream_engine_base_t::timer_event (int id_)
     } else if (id_ == heartbeat_ivl_timer_id) {
         _next_msg = &stream_engine_base_t::produce_ping_message;
         out_event ();
-        if (true)
+        if (!_input_stopped && !_output_stopped)
             add_timer (_options.heartbeat_interval, heartbeat_ivl_timer_id);
     } else if (id_ == heartbeat_ttl_timer_id) {
         _has_ttl_timer = false;
