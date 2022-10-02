0d49eddf16e55cbbc5d106458584fa5f81b43eed
Stephan Guilloux (cos) <stephan.guilloux@crisalid.com>
2022-10-02 02:27:12 +0200

    Problem: Android build script may fail silently
    Solution: Use `set -e`
    
    Note:
    To be reported in ZPROJECT, when generating the same for CZMQ & ZYRE.
    
    Note:
    `make clean` may fail when Makefile is not yet generated
    (case of 1st call of build.sh after git clone).
    
    Additionnaly, cleaned the dependency folder
    (report of ZPROJECT generated scripts).

--- builds/android/ci_build.sh
+++ builds/android/ci_build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+#
+#   Exit if any step fails
+set -e
 
 export NDK_VERSION=android-ndk-r25
 export ANDROID_NDK_ROOT="/tmp/${NDK_VERSION}"
+
+# Cleanup.
+rm -rf /tmp/tmp-deps
+mkdir -p /tmp/tmp-deps
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
