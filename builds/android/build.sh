zeromq/libzmq

commit 65b33ebefa53cec87050acdc8db933831f4fb896

Problem: Android APP fails to load ZMQ since NDK r25.x (#4437)
* Problem: Android APP fails to load ZMQ since NDK r25.x

With the help of the dump of ./configure options (former PR):
```
LIBZMQ (arm) - ./configure options to build 'LIBZMQ':
  > --quiet
  > TOOLCHAIN=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64
  > CC=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang
  > CXX=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang++
  > LD=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/ld
  > AS=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-as
  > AR=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
  > RANLIB=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
  > STRIP=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip
  > CFLAGS= -D_GNU_SOURCE -D_REENTRANT -D_THREAD_SAFE
  > CPPFLAGS= -I/home/stephan/git/zproject-android-testing/libzmq/builds/android/prefix/arm/include
  > CXXFLAGS=
  > LDFLAGS=-L/home/stephan/git/zproject-android-testing/libzmq/builds/android/prefix/arm/lib -L/tmp/android-ndk-r25/sour\
ces/cxx-stl/llvm-libc++/libs/armeabi-v7a
  > LIBS=-lc -ldl -lm -llog -lc++_shared
  > PKG_CONFIG_LIBDIR=/tmp/android-ndk-r25/prebuilt/linux-x86_64/lib/pkgconfig
  > PKG_CONFIG_PATH=/home/stephan/git/zproject-android-testing/libzmq/builds/android/prefix/arm/lib/pkgconfig
  > PKG_CONFIG_SYSROOT_DIR=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/sysroot
  > PKG_CONFIG_DIR=
  > --with-sysroot=/tmp/android-ndk-r25/toolchains/llvm/prebuilt/linux-x86_64/sysroot
  > --host=arm-linux-androideabi
  > --prefix=/home/stephan/git/zproject-android-testing/libzmq/builds/android/prefix/arm
  > --disable-curve
  > --without-docs
```

We can observe that LDFLAGS has invalid `-L<path_to_libc++_shared.so>`:
```

builds/android/build.sh
@@ -14,7 +14,10 @@ ANDROID_BUILD_DIR="${ANDROID_BUILD_DIR:-`pwd`}"
 source ./android_build_helper.sh
 
 # Choose a C++ standard library implementation from the ndk
-ANDROID_BUILD_CXXSTL="gnustl_shared_49"
+export ANDROID_BUILD_CXXSTL="gnustl_shared_49"
+
+# Additional flags for LIBTOOL, for LIBZMQ and other dependencies.
+export LIBTOOL_EXTRA_LDFLAGS='-avoid-version'
 
 BUILD_ARCH=$1
 if [ -z $BUILD_ARCH ]; then
@@ -93,8 +96,6 @@ fi
 ##
 # Build libzmq from local source
 
-LIBTOOL_EXTRA_LDFLAGS='-avoid-version'
-
 (android_build_verify_so ${VERIFY} &> /dev/null) || {
     rm -rf "${cache}/libzmq"
     (cp -r ../.. "${cache}/libzmq" && cd "${cache}/libzmq" && ( make clean || : ))
@@ -116,7 +117,12 @@ LIBTOOL_EXTRA_LDFLAGS='-avoid-version'
 }
 
 ##
+# Fetch the STL as well.
+
+cp "${ANDROID_STL_ROOT}/${ANDROID_STL}" "${ANDROID_BUILD_PREFIX}/lib/."
+
+##
 # Verify shared libraries in prefix
 
-android_build_verify_so ${VERIFY}
+android_build_verify_so "${VERIFY}" "${ANDROID_STL}"
 echo "libzmq android build succeeded"

builds/android/android_build_helper.sh
@@ -104,6 +104,18 @@ function android_build_set_env {
        export ANDROID_BUILD_SYSROOT="${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${HOST_PLATFORM}/sysroot"
     fi
     export ANDROID_BUILD_PREFIX="${ANDROID_BUILD_DIR}/prefix/${TOOLCHAIN_ARCH}"
+
+    # Since NDK r25, libc++_shared.so is no more in 'sources/cxx-stl/...'
+    export ANDROID_STL="libc++_shared.so"
+    if [ -x "${ANDROID_NDK_ROOT}/sources/cxx-stl/llvm-libc++/libs/${TOOLCHAIN_ABI}/${ANDROID_STL}" ] ; then
+        export ANDROID_STL_ROOT="${ANDROID_NDK_ROOT}/sources/cxx-stl/llvm-libc++/libs/${TOOLCHAIN_ABI}"
+    else 
+        export ANDROID_STL_ROOT="${ANDROID_BUILD_SYSROOT}/usr/lib/${TOOLCHAIN_HOST}"
+
+        # NDK 25 requires -L<path-to-libc.so> ... 
+        # I don't understand why, but without it, ./configure fails to build a valid 'conftest'.
+        export ANDROID_LIBC_ROOT="${ANDROID_BUILD_SYSROOT}/usr/lib/${TOOLCHAIN_HOST}/${MIN_SDK_VERSION}"
+    fi
 }
 
 function android_build_env {
@@ -148,6 +160,16 @@ function android_build_env {
     if [ ! -d "$ANDROID_NDK_ROOT" ]; then
         ANDROID_BUILD_FAIL+=("The ANDROID_NDK_ROOT directory does not exist")
         ANDROID_BUILD_FAIL+=("  ${ANDROID_NDK_ROOT}")
+    fi
+
+    if [ ! -d "$ANDROID_STL_ROOT" ]; then
+        ANDROID_BUILD_FAIL+=("The ANDROID_STL_ROOT directory does not exist")
+        ANDROID_BUILD_FAIL+=("  ${ANDROID_STL_ROOT}")
+    fi
+
+    if [ -n "${ANDROID_LIBC_ROOT}" ] && [ ! -d "${ANDROID_LIBC_ROOT}" ]; then
+        ANDROID_BUILD_FAIL+=("The ANDROID_LIBC_ROOT directory does not exist")
+        ANDROID_BUILD_FAIL+=("  ${ANDROID_LIBC_ROOT}")
     fi
 
     if [ ! -d "$TOOLCHAIN_PATH" ]; then
@@ -258,7 +280,10 @@ function android_build_opts {
         local LIBS="-lc -lgcc -ldl -lm -llog -lc++_shared"
     fi
     local LDFLAGS="-L${ANDROID_BUILD_PREFIX}/lib"
-    LDFLAGS+=" -L${ANDROID_NDK_ROOT}/sources/cxx-stl/llvm-libc++/libs/${TOOLCHAIN_ABI}"
+    if [ -n "${ANDROID_LIBC_ROOT}" ] ; then
+	LDFLAGS+=" -L${ANDROID_LIBC_ROOT}"
+    fi
+    LDFLAGS+=" -L${ANDROID_STL_ROOT}"
     CFLAGS+=" -D_GNU_SOURCE -D_REENTRANT -D_THREAD_SAFE"
     CPPFLAGS+=" -I${ANDROID_BUILD_PREFIX}/include"
 

commit 3022e7e099ecad0d684f44613c234edda93425b1

Regression: Android build fails with libsodium.
Scenario:
```
export CURL=libsodium
cd zyre/builds/android
./ci_build.sh
```

Result:
```
Android (arm) build failed for the following reasons:
  Found no library named libzmq.so libsodium.so
    /home/stephan/git/zproject-android-testing/libzmq/builds/android/prefix/arm/lib/libzmq.so libsodium.so
```

Caused by PR #4437, where the 2nd commit was to fix Sonatype findings.

Lesson learnt: Not always a good idea to add double quotes around variables ...

Solution: Make VERIFY an array, so that Sonatype won't complain.

builds/android/build.sh
@@ -55,12 +55,12 @@ if [[ $ANDROID_BUILD_CLEAN ]]; then
     rm -rf "${ANDROID_BUILD_PREFIX}"/*
 fi
 
+VERIFY=("libzmq.so")
 if [ -z $CURVE ]; then
     CURVE="--disable-curve"
-    VERIFY="libzmq.so"
 elif [ $CURVE == "libsodium" ]; then
     CURVE="--with-libsodium=yes"
-    VERIFY="libzmq.so libsodium.so"
+    VERIFY+=("libsodium.so")
     ##
     # Build libsodium from latest master branch
 
@@ -84,13 +84,12 @@ elif [ $CURVE == "libsodium" ]; then
 elif [ $CURVE == "tweetnacl" ]; then
     # Default
     CURVE=""
-    VERIFY="libzmq.so"
 fi
 
 ##
 # Build libzmq from local source
 
-(android_build_verify_so ${VERIFY} &> /dev/null) || {
+(android_build_verify_so "${VERIFY[@]}" &> /dev/null) || {
     rm -rf "${cache}/libzmq"
     (cp -r ../.. "${cache}/libzmq" && cd "${cache}/libzmq" && ( make clean || : ))
 
@@ -118,5 +117,5 @@ cp "${ANDROID_STL_ROOT}/${ANDROID_STL}" "${ANDROID_BUILD_PREFIX}/lib/."
 ##
 # Verify shared libraries in prefix
 
-android_build_verify_so "${VERIFY}" "${ANDROID_STL}"
+android_build_verify_so "${VERIFY[@]}" "${ANDROID_STL}"
 echo "LIBZMQ (${BUILD_ARCH}) - Android build successful"