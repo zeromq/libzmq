41ee05379c1b3538e41e595370d71ff6c7defc3a
Matheus C. França <matheus-catarino@hotmail.com>
2023-02-21 15:24:24 -0300

    more platform.hpp config - enabling windows & macos build

--- build.zig
+++ build.zig
@@ -11,49 +11,76 @@ pub fn build(b: *Builder) void {
     const shared = b.option(bool, "shared", "Build the Shared Library [default: false]") orelse false;
 
     // Generating "platform.hpp"
-    const config_header = if (!target.isWindows()) b.addConfigHeader(.{
-        .style = .blank,
-        .include_path = "platform.hpp",
-    }, .{
-        .ZMQ_USE_EPOLL = 1,
-        .ZMQ_HAVE_CURVE = 1,
-        .ZMQ_USE_TWEETNACL = 1,
-        .ZMQ_HAVE_EVENTFD = 1,
-        .ZMQ_HAVE_IFADDRS = 1,
-        .ZMQ_HAVE_SOCK_CLOEXEC = 1,
-        .ZMQ_HAVE_SO_KEEPALIVED = 1,
-        .ZMQ_HAVE_SO_PEERCRED = 1,
-        .ZMQ_HAVE_TCP_KEEPCNT = 1,
-        .ZMQ_HAVE_TCP_KEEPIDLE = 1,
-        .ZMQ_HAVE_TCP_KEEPINTVL = 1,
-        .ZMQ_HAVE_UIO = 1,
-        .ZMQ_USE_BUILTIN_SHA1 = 1,
-        .HAVE_FORK = 1,
-        .HAVE_POSIX_MEMALIGN = 1,
-        .HAVE_ACCEPT4 = 1,
-        .HAVE_STRNLEN = 1,
-        .ZMQ_IOTHREAD_POLLER_USE_EPOLL = 1,
-        .ZMQ_USE_CV_IMPL_STL11 = 1,
-        .ZMQ_POLL_BASED_ON_POLL = 1,
-        .ZMQ_CACHELINE_SIZE = 64,
-    }) else b.addConfigHeader(.{
-        .style = .blank,
-        .include_path = "platform.hpp",
-    }, .{
-        .ZMQ_HAVE_CURVE = 1,
-        .ZMQ_USE_TWEETNACL = 1,
-        .ZMQ_USE_SELECT = 1,
-        .ZMQ_USE_CV_IMPL_STL11 = 1,
-        .ZMQ_CACHELINE_SIZE = 1,
-        .ZMQ_IOTHREAD_POLLER_USE_SELECT = 1,
-        .ZMQ_POLL_BASED_ON_SELECT = 1,
-        .ZMQ_USE_BUILTIN_SHA1 = 1,
-        .HAVE_STRNLEN = 1,
-    });
-
-    var sources_cpp = std.ArrayList([]const u8).init(b.allocator);
-    sources_cpp.appendSlice(sources) catch @panic("slice error");
-    //if (target.getOsTag() == .windows) sources_cpp.appendSlice(msvc_sources) catch @panic("slice error");
+    const config_header = switch (target.getOsTag()) {
+        .linux => b.addConfigHeader(.{
+            .style = .blank, //.{ .cmake = .{ .path = "builds/cmake/platform.hpp.in" } },
+            .include_path = "platform.hpp",
+        }, .{
+            .ZMQ_HAVE_LINUX = {},
+            .ZMQ_USE_EPOLL = 1,
+            .ZMQ_HAVE_CURVE = 1,
+            .ZMQ_USE_TWEETNACL = 1,
+            .ZMQ_HAVE_EVENTFD = 1,
+            .ZMQ_HAVE_IFADDRS = 1,
+            .ZMQ_HAVE_SOCK_CLOEXEC = 1,
+            .ZMQ_HAVE_SO_BINDTODEVICE = 1,
+            .ZMQ_HAVE_SO_KEEPALIVED = 1,
+            .ZMQ_HAVE_SO_PEERCRED = 1,
+            .ZMQ_HAVE_TCP_KEEPCNT = 1,
+            .ZMQ_HAVE_TCP_KEEPIDLE = 1,
+            .ZMQ_HAVE_TCP_KEEPINTVL = 1,
+            .ZMQ_HAVE_UIO = 1,
+            .ZMQ_HAVE_STRLCPY = 1,
+            .ZMQ_USE_BUILTIN_SHA1 = 1,
+            .HAVE_FORK = 1,
+            .HAVE_POSIX_MEMALIGN = 1,
+            .HAVE_ACCEPT4 = 1,
+            .HAVE_STRNLEN = 1,
+            .ZMQ_IOTHREAD_POLLER_USE_EPOLL = 1,
+            .ZMQ_USE_CV_IMPL_STL11 = 1,
+            .ZMQ_POLL_BASED_ON_POLL = 1,
+            .ZMQ_CACHELINE_SIZE = 64,
+        }),
+        .windows => b.addConfigHeader(.{
+            .style = .blank,
+            .include_path = "platform.hpp",
+        }, .{
+            .ZMQ_HAVE_WINDOWS = {},
+            .ZMQ_HAVE_MINGW32 = {},
+            .ZMQ_HAVE_CURVE = 1,
+            .ZMQ_USE_TWEETNACL = 1,
+            .ZMQ_USE_SELECT = 1,
+            .ZMQ_USE_CV_IMPL_STL11 = 1,
+            .ZMQ_CACHELINE_SIZE = 64,
+            .ZMQ_IOTHREAD_POLLER_USE_SELECT = 1,
+            .ZMQ_POLL_BASED_ON_SELECT = 1,
+            .ZMQ_USE_BUILTIN_SHA1 = 1,
+            .HAVE_STRNLEN = 1,
+        }),
+        .macos => b.addConfigHeader(.{
+            .style = .blank,
+            .include_path = "platform.hpp",
+        }, .{
+            .ZMQ_HAVE_OSX = {},
+            .ZMQ_USE_KQUEUE = 1,
+            .ZMQ_POSIX_MEMALIGN = 1,
+            .ZMQ_CACHELINE_SIZE = 64,
+            .ZMQ_HAVE_CURVE = 1,
+            .ZMQ_USE_TWEETNACL = 1,
+            .ZMQ_HAVE_UIO = 1,
+            .ZMQ_HAVE_IFADDRS = 1,
+            .ZMQ_HAVE_OS_KEEPALIVE = 1,
+            .ZMQ_HAVE_TCP_KEEPALIVE = 1,
+            .ZMQ_HAVE_TCP_KEEPCNT = 1,
+            .ZMQ_HAVE_TCP_KEEPINTVL = 1,
+            .ZMQ_USE_BUILTIN_SHA1 = 1,
+            .ZMQ_IOTHREAD_POLLER_USE_KQEUE = 1,
+            .ZMQ_USE_CV_IMPL_STL11 = 1,
+            .HAVE_STRNLEN = 1,
+            .HAVE_FORK = 1,
+        }),
+        else => b.addConfigHeader(.{}, .{}),
+    };
 
     const libzmq = if (!shared) b.addStaticLibrary(.{
         .name = "zmq",
@@ -75,9 +102,16 @@ pub fn build(b: *Builder) void {
     libzmq.addConfigHeader(config_header);
     libzmq.addIncludePath("include");
     libzmq.addIncludePath("src");
+    libzmq.addIncludePath("src/external");
     libzmq.addIncludePath(config_header.include_path);
-    for (sources_cpp.items) |src| {
-        libzmq.addCSourceFile(src, cxxflags);
+    libzmq.addCSourceFiles(cxxSources, cxxFlags);
+    libzmq.addCSourceFiles(extraCsources, cFlags);
+    if (target.isWindows()) {
+        libzmq.addCSourceFile("external/wepoll/wepoll.c", cFlags);
+        libzmq.linkSystemLibraryName("ws2_32");
+        libzmq.linkSystemLibraryName("rpcrt4");
+        libzmq.linkSystemLibraryName("iphlpapi");
+        //libzmq.linkSystemLibraryName("rt");
     }
     libzmq.linkLibCpp(); // LLVM libc++ (builtin)
     libzmq.linkLibC(); // OS libc
@@ -85,12 +119,13 @@ pub fn build(b: *Builder) void {
     libzmq.installHeadersDirectory("include", "");
 }
 
-const cxxflags: []const []const u8 = &.{
+const cFlags: []const []const u8 = &.{
     "-Oz",
     "-Wall",
     "-pedantic",
 };
-const sources: []const []const u8 = &.{
+const cxxFlags = cFlags ++ [_][]const u8{"-std=c++14"};
+const cxxSources: []const []const u8 = &.{
     "src/kqueue.cpp",
     "src/lb.cpp",
     "src/mailbox.cpp",
@@ -167,8 +202,8 @@ const sources: []const []const u8 = &.{
     "src/ip_resolver.cpp",
     "src/zap_client.cpp",
     "src/zmtp_engine.cpp",
+};
+const extraCsources: []const []const u8 = &.{
     "src/tweetnacl.c",
-
-    //"external/wepoll/wepoll.c",
     "external/sha1/sha1.c",
 };
