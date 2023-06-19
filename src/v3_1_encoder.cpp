f20094c8e2cafeacfb3cc09af66c828a6a1bd4c2
Cornelius Soell <cornelius.soell@rohde-schwarz.com>
2023-06-19 18:00:04 +0200

    Problem: long flag isn't set for subscriptions if topic has between 246 and 255 characters
    Solution: fix V3.1 encoder to calculate long flag after evaluating the subscribe and cancel commands

--- src/v3_1_encoder.cpp
+++ src/v3_1_encoder.cpp
@@ -29,8 +29,6 @@ void zmq::v3_1_encoder_t::message_ready ()
     protocol_flags = 0;
     if (in_progress ()->flags () & msg_t::more)
         protocol_flags |= v2_protocol_t::more_flag;
-    if (in_progress ()->size () > UCHAR_MAX)
-        protocol_flags |= v2_protocol_t::large_flag;
     if (in_progress ()->flags () & msg_t::command
         || in_progress ()->is_subscribe () || in_progress ()->is_cancel ()) {
         protocol_flags |= v2_protocol_t::command_flag;
@@ -39,6 +37,10 @@ void zmq::v3_1_encoder_t::message_ready ()
         else if (in_progress ()->is_cancel ())
             size += zmq::msg_t::cancel_cmd_name_size;
     }
+    // Calculate large_flag after command_flag. Subscribe or cancel commands
+    // increase the message size.
+    if (size > UCHAR_MAX)
+        protocol_flags |= v2_protocol_t::large_flag;
 
     //  Encode the message length. For messages less then 256 bytes,
     //  the length is encoded as 8-bit unsigned integer. For larger
