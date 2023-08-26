d3d97ee3d62f67f7385e8a84765ad31dac4783c0
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-08-26 17:53:24 +0100

    Replace `egrep` with `grep -E`
    For the GNU Grep package version >= 3.8, the `egrep` command emits:
    ```
    egrep: warning: egrep is obsolescent; using grep -E
    ```
    which makes the `./autogen.sh` script very noisy.

--- version.sh
+++ version.sh
@@ -7,9 +7,9 @@ if [ ! -f include/zmq.h ]; then
     echo "version.sh: error: include/zmq.h does not exist" 1>&2
     exit 1
 fi
-MAJOR=`egrep '^#define +ZMQ_VERSION_MAJOR +[0-9]+$' include/zmq.h`
-MINOR=`egrep '^#define +ZMQ_VERSION_MINOR +[0-9]+$' include/zmq.h`
-PATCH=`egrep '^#define +ZMQ_VERSION_PATCH +[0-9]+$' include/zmq.h`
+MAJOR=`grep -E '^#define +ZMQ_VERSION_MAJOR +[0-9]+$' include/zmq.h`
+MINOR=`grep -E '^#define +ZMQ_VERSION_MINOR +[0-9]+$' include/zmq.h`
+PATCH=`grep -E '^#define +ZMQ_VERSION_PATCH +[0-9]+$' include/zmq.h`
 if [ -z "$MAJOR" -o -z "$MINOR" -o -z "$PATCH" ]; then
     echo "version.sh: error: could not extract version from include/zmq.h" 1>&2
     exit 1
