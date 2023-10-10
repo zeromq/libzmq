c4aa008b43651cc4b93809b7ba3a7cc309f376ed
jamesdillonharvey <jamesdillonharvey@gmail.com>
2023-10-10 11:28:56 +0000

    clang-format

--- src/stream_engine_base.cpp
+++ src/stream_engine_base.cpp
@@ -295,10 +295,10 @@ bool zmq::stream_engine_base_t::in_event_internal ()
     if (rc == -1) {
         if (errno != EAGAIN) {
             // In cases where the src/dst have the same IP and the dst uses an ephemeral port, reconnection
-	    // eventually results in the src and dest IP and port clashing (google tcp self connection)
-	    // While this is a protocol_error (you have the single zmq socket handshaking with itself)
-	    // we do not want to to stop reconnection from happening 
-            if(!_endpoint_uri_pair.clash()){
+            // eventually results in the src and dest IP and port clashing (google tcp self connection)
+            // While this is a protocol_error (you have the single zmq socket handshaking with itself)
+            // we do not want to to stop reconnection from happening
+            if(!_endpoint_uri_pair.clash ()){
                 error (protocol_error);
                 return false;
             }
