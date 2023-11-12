f82573dbeec3f1e792d671a4ab61dccc88e4ecb7
Axel Rietschin <axelriet@gmail.com>
2023-11-12 02:25:31 -0800

    More testfixes

--- tests/test_pubsub.cpp
+++ tests/test_pubsub.cpp
@@ -3,6 +3,14 @@
 #include "testutil.hpp"
 #include "testutil_unity.hpp"
 
+#if defined ZMQ_HAVE_OPENPGM && defined ZMQ_HAVE_WINDOWS
+#include <shlobj_core.h>
+#pragma comment(lib, "shell32.lib")
+#include <winsock2.h>
+#include <iphlpapi.h>
+#pragma comment(lib, "iphlpapi.lib")
+#endif
+
 SETUP_TEARDOWN_TESTCONTEXT
 
 void test (const char *address)
@@ -43,16 +51,75 @@ void test (const char *address)
 void test_norm ()
 {
 #if defined ZMQ_HAVE_NORM
-    test (
-      "norm://224.0.1.20:6210"); // IANA: experiment.mcast.net (any private experiment)
+    test ("norm://224.0.1.20:6210");
 #else
     TEST_IGNORE_MESSAGE ("libzmq without NORM, ignoring test.");
 #endif
 }
 
 #if defined ZMQ_HAVE_OPENPGM
-#if defined(ZMQ_HAVE_WINDOWS)
-// #define NETWORK_ADAPTER "xxx.xxx.xxx.xxx"
+#if defined ZMQ_HAVE_WINDOWS
+int GetAdapterIpAddress (
+  _Out_writes_bytes_ (ipAddressBufferSizeBytes) char *ipAddressBuffer,
+  size_t ipAddressBufferSizeBytes)
+{
+    ULONG bufferSizeBytes = 0;
+
+    if (ipAddressBufferSizeBytes < 8) {
+        return -1;
+    }
+    
+    *ipAddressBuffer = 0;
+
+    //
+    // First call retrieves the needed buffer size in bytes
+    //
+
+    if (GetAdaptersInfo (NULL, &bufferSizeBytes) != ERROR_BUFFER_OVERFLOW) {
+        return -1;
+    }
+
+    void* buffer = malloc (bufferSizeBytes);
+
+    if (!buffer) {
+        return -1;
+    }
+
+    PIP_ADAPTER_INFO pAdapterInfo = (IP_ADAPTER_INFO *) buffer;
+
+    //
+    // Retrieve information for all adapters
+    //
+
+    if (GetAdaptersInfo (pAdapterInfo, &bufferSizeBytes) != ERROR_SUCCESS) {
+        free (buffer);
+        return -1;
+    }
+
+    //
+    // Find the first Ethernet adapter with an IP address assigned that is not private
+    //
+
+    while (pAdapterInfo) {
+        if (pAdapterInfo->Type == MIB_IF_TYPE_ETHERNET) {
+            if (pAdapterInfo->IpAddressList.IpAddress.String[0] != '0'
+                && strncmp (pAdapterInfo->IpAddressList.IpAddress.String, "172",
+                            3)
+                && strncmp (pAdapterInfo->IpAddressList.IpAddress.String, "192",
+                            3)) {
+                strcpy_s (ipAddressBuffer, ipAddressBufferSizeBytes,
+                          pAdapterInfo->IpAddressList.IpAddress.String);
+                free (buffer);
+                return 0;
+            }
+        }
+        pAdapterInfo = pAdapterInfo->Next;
+    }
+
+    free (buffer);
+    return -1;
+}
+
 #else
 #define NETWORK_ADAPTER "eth0"
 #endif
@@ -61,12 +128,21 @@ void test_norm ()
 void test_epgm ()
 {
 #if defined ZMQ_HAVE_OPENPGM
-#ifdef NETWORK_ADAPTER
-    test (
-      "epgm://" NETWORK_ADAPTER
-      ";224.0.1.20:6211"); // IANA: experiment.mcast.net (any private experiment)
+#ifdef ZMQ_HAVE_WINDOWS
+    char network[256];
+    char ip_address[64];
+    TEST_ASSERT_EQUAL_INT (
+      0, GetAdapterIpAddress (ip_address, _countof (ip_address)));
+    sprintf_s (network, _countof (network), "epgm://%s;224.0.1.20:6211",
+               ip_address);
+    test (network);
 #else
-    TEST_IGNORE_MESSAGE ("libzmq with OpenPGM, but NETWORK_ADAPTER wasn't set, ignoring test.");
+#ifdef NETWORK_ADAPTER
+    test ("epgm://" NETWORK_ADAPTER ";224.0.1.20:6211");
+#else
+    TEST_IGNORE_MESSAGE (
+      "libzmq with OpenPGM, but NETWORK_ADAPTER wasn't set, ignoring test.");
+#endif
 #endif
 #else
     TEST_IGNORE_MESSAGE ("libzmq without OpenPGM, ignoring test.");
@@ -76,13 +152,26 @@ void test_epgm ()
 void test_pgm ()
 {
 #if defined ZMQ_HAVE_OPENPGM
+#ifdef ZMQ_HAVE_WINDOWS
+    if (!IsUserAnAdmin ()) {
+        TEST_IGNORE_MESSAGE (
+          "libzmq with OpenPGM, but user is not an admin, ignoring test.");
+    } else {
+        char network[256];
+        char ip_address[64];
+        TEST_ASSERT_EQUAL_INT (
+          0, GetAdapterIpAddress (ip_address, _countof (ip_address)));
+        sprintf_s (network, _countof (network), "pgm://%s;224.0.1.20:6212",
+                   ip_address);
+        test (network);
+    }
+#else
 #ifdef NETWORK_ADAPTER
-    test (
-      "pgm://" NETWORK_ADAPTER
-      ";224.0.1.20:6212"); // IANA: experiment.mcast.net (any private experiment)
+    test ("pgm://" NETWORK_ADAPTER ";224.0.1.20:6212");
 #else
     TEST_IGNORE_MESSAGE (
       "libzmq with OpenPGM, but NETWORK_ADAPTER wasn't set, ignoring test.");
+#endif
 #endif
 #else
     TEST_IGNORE_MESSAGE ("libzmq without OpenPGM, ignoring test.");
